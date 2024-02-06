b7110681093d2df925cad8990b26fc6a78f47d28
ideen1 <ibanijamali@gmail.com>
2024-02-06 00:42:38 -0700

    Commit Repo configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const prettierConfig = require("./prettier.config.cjs");
+
+const project = path.join(__dirname, "./src/renderer/tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:promise/recommended",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project,
+      },
+      rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-explicit-any": "warn",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project,
+  },
+  plugins: ["@typescript-eslint", "prettier", "promise"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    eqeqeq: ["error", "smart"],
+    "prettier/prettier": ["error", prettierConfig],
+    "no-eval": "error",
+    "no-var": "error",
+    // "prettier/prettier": "error",
+    "no-restricted-imports": ["error", { patterns: [".*"] }],
+    "tailwindcss/no-custom-classname": "off",
+    "tailwindcss/classnames-order": "off", // tcss prettier plugin handles this
+  },
+};
+
+module.exports = config;
