fd2ec352290c7cf69df92275709827a2ebd56b97
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-16 18:14:20 -0700

    Update Footer colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,15 @@
 import SocialMediaIcons from "@/components/SocialMediaIcons";
 
 const footerContainer =
-  "flex flex-col items-center justify-center text-[#A689FF] h-36 bg-[#FFFFFF] md:bg-[#7055FD]";
+  "flex flex-col items-center justify-center text-awesomer-purple h-36 bg-white bg-awesome-purple";
 
 const Footer = () => {
   return (
     <>
       <div className={footerContainer}>
-        <h2 className="font-medium">Keep Up With Us!</h2>
-        <SocialMediaIcons classes="text-[#7055FD] md:text-white" />
-        <h4 className="mt-6 font-light text-[#C5C5C5]">
+        <h2 className="font-bold">Keep Up With Us!</h2>
+        <SocialMediaIcons classes=" text-white" />
+        <h4 className="mt-6 font-medium text-ehhh-grey">
           Copyright @ Code the Change YYC
         </h4>
       </div>
