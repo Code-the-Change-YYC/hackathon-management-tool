3318c625992763a707dd401aa8dab0c555763b0d
Burton Jong <burtonjong05@gmail.com>
2024-02-20 19:41:40 -0700

    First commit. Added the component above the footer cause i had no idea where else to put it. Imageplaceholder also not in correct folder because eslint wouldnt let me commit otherwise

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 
 import Footer from "@/components/Footer";
+import JudgeShowcase from "@/components/JudgeShowcase";
 
 interface Props {
   children: ReactNode | ReactNode[];
@@ -10,8 +11,9 @@ export default function MainLayout({ children }: Props) {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {children}
+        \{children}
       </main>
+      <JudgeShowcase />
       <Footer />
     </>
   );
