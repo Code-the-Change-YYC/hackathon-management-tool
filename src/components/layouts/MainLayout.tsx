2a554fb03a9aacc30126001a4479c82cb7b9062e
Burton Jong <burtonjong05@gmail.com>
2024-02-22 17:44:26 -0700

    New folder for images and putting component in page.tsx

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -14,7 +14,6 @@ export default function MainLayout({ children }: Props) {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         \{children}
       </main>
-      <JudgeShowcase />
       <Footer />
     </>
   );
