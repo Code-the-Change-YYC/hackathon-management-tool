a9d39ffbf6b9a38ff33a8edebf3a63f7d45339f7
Burton Jong <burtonjong05@gmail.com>
2024-02-22 17:44:26 -0700

    New folder for images and putting component in page.tsx

--- src/components/JudgeShowcase.tsx
+++ src/components/JudgeShowcase.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import imageplaceholder from "@/components/imgplaceholder.png";
+const imageplaceholder = "/images/imgplaceholder.png";
 
 const JudgeShowcase = () => {
   type Judge = {
@@ -11,52 +11,52 @@ const JudgeShowcase = () => {
 
   const judges: Judge[] = [
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
       judgeName: "Topan Budiman",
       judgeCompany: "Vena Solutions",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
 
       judgeName: "Ideen Banijamali",
       judgeCompany: "Garmin",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
 
       judgeName: "Cat",
       judgeCompany: "Meow",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
       judgeName: "Topan Budiman",
       judgeCompany: "Vena Solutions",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
 
       judgeName: "Ideen Banijamali",
       judgeCompany: "Garmin",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
 
       judgeName: "Cat",
       judgeCompany: "Meow",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
       judgeName: "Topan Budiman",
       judgeCompany: "Vena Solutions",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
 
       judgeName: "Ideen Banijamali",
       judgeCompany: "Garmin",
     },
     {
-      judgeImg: imageplaceholder.src,
+      judgeImg: imageplaceholder,
 
       judgeName: "Cat",
       judgeCompany: "Meow",
@@ -64,29 +64,31 @@ const JudgeShowcase = () => {
   ];
 
   return (
-    <div className="mx-10 my-5 flex flex-col">
+    <div className="flex flex-col bg-white px-10 py-5">
       <div className="ml-5 flex flex-row">
         <h1 className="text-2xl font-extrabold">Judges</h1>
       </div>
       <div className="mb-5 flex w-full flex-row flex-wrap justify-center">
         {judges.map((judge, index) => (
           <div
-            className="flex w-1/2 flex-row gap-5 py-10 sm:w-1/3 sm:justify-center"
+            className="flex w-1/2 flex-row py-10 sm:w-1/3 sm:justify-center"
             key={index}
           >
-            <div className="relative ml-3 size-24 min-w-24 overflow-hidden rounded-full sm:ml-10 sm:size-36 sm:min-w-36">
-              <Image
-                src={judge.judgeImg}
-                alt="Profile Picture"
-                objectFit="cover"
-                layout="fill"
-              />
-            </div>
-            <div className="flex flex-col justify-center">
-              <p className="font-extrabold text-awesome-purple">
-                {judge.judgeName}
-              </p>
-              <p>{judge.judgeCompany}</p>
+            <div className="flex flex-row gap-3">
+              <div className="relative size-20 min-w-20 overflow-hidden rounded-full sm:size-28 sm:min-w-28">
+                <Image
+                  src={judge.judgeImg}
+                  alt="Profile Picture"
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+              <div className="flex flex-col justify-center">
+                <p className="font-extrabold text-awesome-purple">
+                  {judge.judgeName}
+                </p>
+                <p>{judge.judgeCompany}</p>
+              </div>
             </div>
           </div>
         ))}
