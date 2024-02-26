3450621f4115eda8db5370e98b38e82bb4db72ad
Burton Jong <burtonjong05@gmail.com>
2024-02-26 15:20:38 -0700

    Rebased changes and folder/file movement

--- src/components/LandingPage/JudgeShowcase.tsx
+++ src/components/LandingPage/JudgeShowcase.tsx
@@ -64,7 +64,7 @@ const JudgeShowcase = () => {
   ];
 
   return (
-    <div className="flex flex-col bg-white px-10 py-5">
+    <div className="flex h-screen flex-col justify-center bg-white px-10">
       <div className="flex w-fit flex-col items-center sm:w-1/4 sm:pr-5">
         <h1 className="text-2xl font-extrabold">Judges</h1>
         <svg
