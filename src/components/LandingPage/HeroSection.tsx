b971e89570e79687c2e2642722f62e6cfcf271ae
Natalie <natalie.yeung16@gmail.com>
2024-02-28 12:04:49 -0700

    Add countdown background and css

--- src/components/LandingPage/HeroSection.tsx
+++ src/components/LandingPage/HeroSection.tsx
@@ -4,9 +4,14 @@ import PropTypes from "prop-types";
 
 import HeroSectionBackground from "/public/images/landingpage/hero_section_background.png";
 
-const heroSectionStyles = "flex flex-col -mt-5";
-const heroTileStyles = "mx-10 -mt-20 md:m-0 ";
+const heroSectionStyles =
+  "flex flex-col relative flex h-screen items-center justify-center";
+// const heroTileStyles = "mx-10 md:m-0 ";
+const heroTileStyles = "mx-10 mt:m-72 md:mt-10  ";
 
+const linkStyles = "my-5 flex justify-start font-bold md:justify-center";
+const webPageContainerStyles =
+  "bg-[#00D3A9] flex justify-center items-center rounded-[5px] md:w-[1000px] mx-auto max-h-[100vh] p-24";
 interface HeroTileProps {
   eventName: string;
   eventYear: string;
@@ -33,7 +38,7 @@ const HeroTile: React.FC<HeroTileProps> = ({
     <div className={heroTileStyles}>
       <div>
         <h1
-          className="-mt-20 text-7xl font-black text-[#FFFF] drop-shadow-lg md:m-0 md:text-center md:text-7xl"
+          className="text-7xl font-black text-[#FFFF] drop-shadow-lg md:m-0 md:text-center md:text-7xl"
           style={{
             textShadow: `
     -2px -2px 0 #7055FD, 
@@ -51,17 +56,29 @@ const HeroTile: React.FC<HeroTileProps> = ({
           {eventName}
           <span className="text-[#BAFBE4]"> {eventYear}</span>
         </h1>
-        <strong className="my-8 flex flex-wrap justify-center text-2xl text-[#7055FD] md:px-40 md:text-center lg:px-72">
+        <strong className="my-8 flex flex-wrap justify-center text-2xl text-[#7055FD] opacity-95 md:px-40 md:text-center lg:px-72">
           {eventBlurb}
         </strong>
       </div>
-      <div className="flex justify-start md:justify-center">
+      <div className={linkStyles}>
         <Link href="/" legacyBehavior>
-          <div className=" cursor-pointer rounded-3xl border-4 border-white bg-[#7055FD] p-4 text-2xl font-bold text-white opacity-90 hover:opacity-80">
+          <div className=" cursor-pointer rounded-3xl border-4 border-white bg-[#7055FD] p-4 text-2xl text-white opacity-90 hover:opacity-70">
             Join Hackathon
           </div>
         </Link>
       </div>
+      <div className={linkStyles}>
+        <p>
+          Already registered?
+          <Link href="/" legacyBehavior>
+            <span className="cursor-pointer text-[#7055FD] opacity-90 hover:opacity-70">
+              {" "}
+              Sign in
+            </span>
+          </Link>
+        </p>
+      </div>
+      <div className={webPageContainerStyles}>hello</div>
     </div>
   );
 };
@@ -77,20 +94,18 @@ HeroTile.propTypes = {
 const HeroSection = () => {
   return (
     <div className={heroSectionStyles}>
-      <div className="relative flex h-screen items-center justify-center">
-        <Image
-          src={HeroSectionBackground}
-          alt="Landing page background"
-          layout="fill"
-          placeholder="blur"
-          objectFit="cover"
-        />
-        {HERO_SECTION.map((values, index) => (
-          <div key={index}>
-            <HeroTile {...values} />
-          </div>
-        ))}
-      </div>
+      <Image
+        src={HeroSectionBackground}
+        alt="Landing page background"
+        layout="fill"
+        placeholder="blur"
+        objectFit="cover"
+      />
+      {HERO_SECTION.map((values, index) => (
+        <div key={index}>
+          <HeroTile {...values} />
+        </div>
+      ))}
     </div>
   );
 };
