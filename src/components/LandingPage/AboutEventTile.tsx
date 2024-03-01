762f3b1664c9533102f74f41b3d22fe2cee05e29
hannagracec <hannacho847@gmail.com>
2024-03-01 00:05:17 -0700

    changed variables to uppercase snakecase

--- src/components/LandingPage/AboutEventTile.tsx
+++ src/components/LandingPage/AboutEventTile.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import type { ReactNode } from "react";
 
-const eventDetailsSectionStyles = "flex flex-col items-center bg-white";
-const eventDetailsContentStyles =
+const EVENT_DETAILS_SECTION_STYLES = "flex flex-col items-center bg-white";
+const EVENT_DETAILS_CONTENT_STYLES =
   "bg-pastel-pink border-[5px] border-dark-pink rounded-[25px] mb-[80px] mt-[50px] md:flex md:justify-center md:max-w-[1100px] md:shadow-[15px_15px_0px_0px_#FF4D6F]";
-const eventImageContainerStyles =
+const EVENT_IMAGE_CONTAINER_STYLES =
   "bg-blackish rounded-t-[20px] border-b-[5px] border-dark-pink w-[290px] h-[250px] md:rounded-tr-none md:rounded-l-[20px] md:border-b-0 md:border-r-[5px] md:h-[370px] md:w-[40vw]";
-const eventImageStyles = "w-full h-full object-cover";
-const eventDetailsContainerStyles =
+const EVENT_IMAGE_STYLES = "w-full h-full object-cover";
+const EVENT_DETAILS_CONTAINER_STYLES =
   "flex items-center w-[290px] h-[370px] rounded-b-[20px] md:w-[50vw] md:rounded-bl-none md:rounded-r-[20px]";
-const eventDetailStyles = "flex items-center ml-[40px]";
-const eventDetailTitleStyles =
+const EVENT_DETAIL_STYLES = "flex items-center ml-[40px]";
+const EVENT_DETAIL_TITLE_STYLES =
   "text-lg text-blackish font-extrabold leading-[1.2]";
-const iconContainerStyles = "bg-white rounded-[12px] p-[5px] m-[15px]";
+const ICON_CONTAINER_STYLES = "bg-white rounded-[12px] p-[5px] m-[15px]";
 
 const icons = {
   date: "/svgs/aboutEventTile/date_icon.svg",
@@ -35,11 +35,11 @@ const eventDetails = {
 };
 
 const EventDetail = ({ iconSrc, iconName, children }: EventDetailProps) => (
-  <div className={eventDetailStyles}>
-    <div className={iconContainerStyles}>
+  <div className={EVENT_DETAIL_STYLES}>
+    <div className={ICON_CONTAINER_STYLES}>
       <Image src={iconSrc} alt={`${iconName} icon`} width={35} height={35} />
     </div>
-    <h2 className={eventDetailTitleStyles}>{children}</h2>
+    <h2 className={EVENT_DETAIL_TITLE_STYLES}>{children}</h2>
   </div>
 );
 
@@ -52,18 +52,18 @@ const AboutEventTile = () => {
   });
 
   return (
-    <div className={eventDetailsSectionStyles}>
-      <div className={eventDetailsContentStyles}>
-        <div className={eventImageContainerStyles}>
+    <div className={EVENT_DETAILS_SECTION_STYLES}>
+      <div className={EVENT_DETAILS_CONTENT_STYLES}>
+        <div className={EVENT_IMAGE_CONTAINER_STYLES}>
           <Image
-            className={eventImageStyles}
+            className={EVENT_IMAGE_STYLES}
             src={eventDetails.eventImg}
             alt={eventDetails.eventLocation}
             width={100}
             height={100}
           />
         </div>
-        <div className={eventDetailsContainerStyles}>
+        <div className={EVENT_DETAILS_CONTAINER_STYLES}>
           <div>
             <EventDetail iconSrc={icons.date} iconName="date">
               {formattedDate}
