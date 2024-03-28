37f5c601d503420f80458aadf2ad78e5e9629cc5
TheSaddestBread <74692833+AllanKoder@users.noreply.github.com>
2024-03-28 10:08:20 -0600

    message authentication code

--- src/app/food/page.tsx
+++ src/app/food/page.tsx
@@ -1,7 +1,18 @@
 // app/food/page.tsx
+import hmac from "js-crypto-hmac";
+
 import { AuthGetCurrentUserServer } from "@/utils/amplify-utils";
 
-export default function FoodPage() {
+// for npm
+
+export default async function FoodPage() {
+  let userVerificationId = null;
+  const key = new Uint8Array([
+    0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef, 0xea, 0x1f, 0x12, 0x34,
+    0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef, 0xea, 0x1f, 0x12, 0x34,
+    0x90, 0xab, 0xcd, 0xef, 0xea, 0x1f, 0x12, 0x34,
+  ]);
+  const hash = "SHA-256";
   async function currentAuthenticatedUser() {
     try {
       const user = await AuthGetCurrentUserServer()
@@ -13,12 +24,28 @@ export default function FoodPage() {
         });
       console.log(`The username: ${user?.username}`);
       console.log(`The userId: ${user?.userId}`);
+
+      console.log(await createAuthenticationCode("ab"));
+      if (user) {
+        userVerificationId = user?.username;
+      }
     } catch (err) {
       console.log(err);
     }
   }
 
-  void currentAuthenticatedUser();
+  function stringToUint8Array(inputString: string) {
+    const encoder = new TextEncoder();
+    return encoder.encode(inputString);
+  }
 
-  return <a>test</a>;
+  async function createAuthenticationCode(message: string) {
+    const msg = stringToUint8Array(message);
+    const messageCode = await hmac.compute(key, msg, hash);
+    return messageCode;
+  }
+
+  await currentAuthenticatedUser();
+
+  return <a>{userVerificationId}</a>;
 }
