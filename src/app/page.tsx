b15d8016a175e1f270ad796e4a23fa38e0e6406e
Burton Jong <burtonjong05@gmail.com>
2024-03-29 19:21:44 -0600

    Adding judge showcase to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,9 @@ export default function Home() {
         <PagePlaceholder />
       ) : (
         <>
+          <AboutEventTile />
           <JudgingCriteria />
+          <JudgeShowcase />
           <ThankSponsors />
         </>
       )}
