1395630b84b46c8f0fd08235d2cc681a822edab4
Burton Jong <burtonjong05@gmail.com>
2024-04-02 15:02:32 -0600

    Packaged up all the input fields to send to the backend

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,13 +1,53 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const LINK_STYLES =
   "align-center text-center text-1xl md:text-md my-12 flex flex-row gap-16 text-[#FF6B54]";
-const INPUT_STYLES =
+const INPUT_STYLES: string =
   "rounded-full border-4 border-white bg-[#FFFFFF] /50 ps-3 py-2 my-2 text-sm md:text-md";
 const BUTTON_STYLES =
   " rounded-full border-4 border-white bg-[#FF6B54] px-10  md:px-12 py-2 my-2 text-white";
 
+type FormState = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  institution: string;
+  wantMeals: boolean;
+  allergies: string;
+  checkInStatus: boolean;
+};
+
 const UserProfile = () => {
+  const [wantMeals, setWantMeals] = useState<boolean>(true);
+
+  const [formState, setFormState] = useState<FormState>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    institution: "",
+    wantMeals: wantMeals,
+    allergies: "",
+    checkInStatus: false,
+  });
+
+  useEffect(() => {
+    setFormState((prevState) => ({ ...prevState, wantMeals: wantMeals }));
+  }, [wantMeals]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const createPost = () => {
+    console.log(formState);
+  };
+
+  const checkedIn = false; // Placeholder value for now
+
   return (
     <div className="flex w-full flex-col bg-[#FFD7C5]">
       <div className="flex h-60 items-center justify-center bg-[#FF6B54]">
@@ -29,7 +69,7 @@ const UserProfile = () => {
           </h1>
           <button className={BUTTON_STYLES}>Edit</button>
         </div>
-        <form className="flex flex-col ">
+        <form className="flex flex-col" action={createPost}>
           <div>
             <div className="grid grid-cols-2 gap-5 ">
               <label>First Name</label>
@@ -40,16 +80,30 @@ const UserProfile = () => {
                 className={INPUT_STYLES}
                 type="text"
                 placeholder="First Name"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  onChange(e)
+                }
+                name="firstName"
               />
               <input
                 className={INPUT_STYLES}
                 type="text"
                 placeholder="Last Name"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  onChange(e)
+                }
+                name="lastName"
               />
             </div>
           </div>
           <label>Email</label>
-          <input className={INPUT_STYLES} type="text" placeholder="Email" />
+          <input
+            className={INPUT_STYLES}
+            type="text"
+            placeholder="Email"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e)}
+            name="email"
+          />
           <label>Password</label>
           <input
             className={INPUT_STYLES}
@@ -61,22 +115,55 @@ const UserProfile = () => {
             className={INPUT_STYLES}
             type="text"
             placeholder="Institution"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e)}
+            name="institution"
           />
           <label>Do you want provided meals at the hackathon?</label>
-          <input className={INPUT_STYLES} type="text" placeholder="Allergies" />
-          <label>Do you have any allergies?</label>
-          <input className={INPUT_STYLES} type="text" placeholder="Allergies" />
+          <select
+            className={INPUT_STYLES}
+            value={wantMeals ? "Yes" : "No"}
+            onChange={(e) =>
+              e.target.value === "Yes"
+                ? setWantMeals(true)
+                : setWantMeals(false)
+            }
+          >
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+          {wantMeals && (
+            <>
+              <label>Do you have any allergies?</label>
+              <input
+                type="text"
+                placeholder="e.g. Dairy, Nuts, etc."
+                className={INPUT_STYLES}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  onChange(e)
+                }
+                name="allergies"
+              />
+            </>
+          )}
           <p>
             Check-in Status <br />
             This status will change to &quot;Yes&quot; after you&apos;ve checked
             in on hackathon day
           </p>
-          <input className={INPUT_STYLES} type="text" placeholder="Check-in" />
+          <input
+            className={INPUT_STYLES}
+            type="text"
+            value={checkedIn ? "Yes" : "No"}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e)}
+            readOnly
+          />
+          <div className=" my-6 flex flex-col justify-between md:flex-row">
+            <button className={BUTTON_STYLES}>Cancel</button>
+            <button type="submit" className={BUTTON_STYLES}>
+              Save
+            </button>
+          </div>
         </form>
-        <div className=" my-6 flex flex-col justify-between md:flex-row">
-          <button className={BUTTON_STYLES}>Cancel</button>
-          <button className={BUTTON_STYLES}>Save</button>
-        </div>
       </div>
     </div>
   );
