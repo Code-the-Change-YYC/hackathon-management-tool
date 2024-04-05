d16be7a0da995a0be901b240f25e4352bdaefcfa
Burton Jong <burtonjong05@gmail.com>
2024-04-05 17:59:00 -0600

    Small changes to user profile navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import AboutEventTile from "@/components/LandingPage/AboutEventTile";
 import JudgeShowcase from "@/components/LandingPage/JudgeShowcase";
 import JudgingCriteria from "@/components/LandingPage/JudgingCriteria";
-import UserProfile from "@/components/UserProfile/UserProfile";
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
         <AboutEventTile />
         <JudgingCriteria />
         <JudgeShowcase />
-        <UserProfile />
       </>
     </main>
   );
