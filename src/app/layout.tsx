d9ed2b14aab21693e536d8b89cb0465452402a69
ideen1 <ibanijamali@gmail.com>
2024-04-25 19:48:23 -0600

    Add TanStack and demo data get

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import ConfigureAmplifyClientSide from "@/components/_Amplify/ConfigureAmplify";
 // import { UserContextProvider } from "@/components/contexts/UserContext";
 import MainLayout from "@/components/layouts/MainLayout";
 import "@aws-amplify/ui-react/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,17 +15,21 @@ export const metadata: Metadata = {
   title: "Hack the Change",
   description: "Hack the Change management tool",
 };
+const queryClient = new QueryClient({});
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ConfigureAmplifyClientSide />
-        {/* <UserContextProvider> */}
-        <MainLayout>{children}</MainLayout>
-        {/* </UserContextProvider> */}
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <body className={inter.className}>
+          <ConfigureAmplifyClientSide />
+          {/* <UserContextProvider> */}
+          <MainLayout>{children}</MainLayout>
+          {/* </UserContextProvider> */}
+        </body>
+      </html>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
