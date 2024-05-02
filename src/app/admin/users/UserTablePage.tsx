49a4d4e7d7314450d55f1488f31f0ce5a316d2b2
Natalie <natalie.yeung16@gmail.com>
2024-05-02 13:50:03 -0600

    Add table data and headers based on user information

--- src/app/admin/users/UserTablePage.tsx
+++ src/app/admin/users/UserTablePage.tsx
@@ -1,9 +1,66 @@
+import DataTableSection from "@/app/admin/components/DataTableSection";
 import DropDownRole from "@/app/admin/components/DropDownRole";
+
+const users = [
+  {
+    lastName: "Last",
+    firstName: "First",
+    role: "Participant",
+    team: "Group Name",
+    email: "email@ucalgary.ca",
+  },
+  {
+    lastName: "Last",
+    firstName: "First",
+    role: "Judge",
+    team: "",
+    email: "email@ucalgary.ca",
+  },
+  {
+    lastName: "Last",
+    firstName: "First",
+    role: "Participant",
+    team: "Group Name",
+    email: "email@ucalgary.ca",
+  },
+  {
+    lastName: "Last",
+    firstName: "First",
+    role: "Judge",
+    team: "",
+    email: "email@ucalgary.ca",
+  },
+  {
+    lastName: "Last",
+    firstName: "First",
+    role: "Participant",
+    team: "Group Name",
+    email: "email@ucalgary.ca",
+  },
+] as const;
+
+// added option for styles to customize the column widths
+const tableHeaders = [
+  { columnHeader: "Last Name", className: "w-1/6" },
+  { columnHeader: "First Name", className: "w-1/6" },
+  { columnHeader: "Role", className: "w-1/6" },
+  { columnHeader: "Team", className: "w-1/6" },
+  { columnHeader: "Email", className: "w-3/4" },
+];
+
+const tableData = users.map((user) => [
+  user.lastName,
+  user.firstName,
+  user.role,
+  user.team,
+  user.email,
+]);
 
 export default function UserTablePage() {
   return (
     <div>
       <DropDownRole />
+      <DataTableSection tableData={tableData} tableHeaders={tableHeaders} />
     </div>
   );
 }
