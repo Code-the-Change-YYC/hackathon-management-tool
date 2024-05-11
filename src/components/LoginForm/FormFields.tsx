335b99abc6787881bc9fb76f8da47dd53042b449
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-10 23:02:37 -0600

    Saves personal info to db

--- src/components/LoginForm/FormFields.tsx
+++ src/components/LoginForm/FormFields.tsx
@@ -9,29 +9,26 @@ const FormFields = ({
   mutationStatus?: MutationStatus;
 }) => {
   return (
-    <>
-      <div
-        className="relative flex w-full flex-col justify-center gap-4 rounded-3xl bg-white
-    "
-      >
-        <Authenticator.SignUp.FormFields />
-        <div className=" flex w-full justify-end ">
-          <button
-            onClick={() => void signOut()}
-            className=" rounded-full border-4 border-white bg-awesomer-purple px-4 py-1 font-semibold text-white shadow-lg hover:opacity-90 md:px-8 md:py-2"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            disabled={mutationStatus === "pending"}
-            className=" rounded-full border-4 border-white bg-awesomer-purple px-4 py-1 font-semibold text-white  shadow-lg hover:opacity-90 md:px-8 md:py-2"
-          >
-            Sign up
-          </button>
-        </div>
+    <div className="relative flex w-full flex-col justify-center gap-4 rounded-3xl bg-white">
+      <Authenticator.SignUp.FormFields />
+      <div className=" flex w-full justify-end ">
+        <button
+          onClick={() => void signOut()}
+          className=" rounded-full border-4 border-white bg-awesomer-purple px-4 py-1 font-semibold text-white shadow-lg hover:opacity-90 md:px-8 md:py-2"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          disabled={
+            mutationStatus === "pending" || mutationStatus === "success"
+          }
+          className=" rounded-full border-4 border-white bg-awesomer-purple px-4 py-1 font-semibold text-white  shadow-lg hover:opacity-90 md:px-8 md:py-2"
+        >
+          Sign up
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 export default FormFields;
--- src/components/LoginForm/PersonalFormFields.tsx
+++ src/components/LoginForm/PersonalFormFields.tsx
@@ -1,20 +1,23 @@
 import { generateClient } from "aws-amplify/api";
+import { useRouter } from "next/navigation";
 
 import type { Schema } from "@/amplify/data/resource";
 import FormFields from "@/components/LoginForm/FormFields";
 import FormFieldsHeader from "@/components/LoginForm/FormFieldsHeader";
 import { Flex, Input, Label, SelectField } from "@aws-amplify/ui-react";
-
-// import { useMutation } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 const client = generateClient<Schema>();
 export default function PersonalFormFields() {
-  // const mutation = useMutation({
-  //   mutationFn: (newUser: CreateUserInput) => {
-  //     console.log(newUser);
-  //     return client.models.User.create(newUser);
-  //   },
-  // });
+  const router = useRouter();
+  const userMutation = useMutation({
+    mutationFn: async (input: Schema["User"]["type"]) => {
+      (await client.models.User.create(input)).data;
+    },
+    onSuccess: () => {
+      router.push("/");
+    },
+  });
   const institutions = [
     "University of Calgary",
     "Mount Royal University",
@@ -22,87 +25,71 @@ export default function PersonalFormFields() {
     "Other",
     "None",
   ];
+  // Not really typesafe, but it's for a hackathon...
   const submitForm = (formData: FormData) => {
-    const data = {
-      firstName: formData.get("first_name") as string,
-      lastName: "string", // .....
-      institution: "string",
-      meals: true,
-      allergies: "string",
-      id: "string",
-      createdAt: "string",
-      updatedAt: "string",
-    };
-    const returnValue = client.models.User.create(
-      data,
-      //   {
-      //   firstName: "string",
-      //   lastName: "string",
-      //   institution: "string",
-      //   meals: true,
-      //   allergies: "string",
-      // }
-    );
-    console.log(returnValue.then((r) => console.log(r)));
+    const data = Object.fromEntries(formData.entries());
+    const user = {
+      ...data,
+      meals: data.meals === "yes" ? true : false,
+    } as Schema["User"]["type"];
+    try {
+      userMutation.mutate(user);
+    } catch (e) {}
   };
   return (
-    <>
-      <form
-        action={submitForm}
-        className="relative flex w-full flex-col justify-center gap-4 rounded-3xl bg-white p-4 md:p-8
-    "
-      >
-        <FormFieldsHeader className={" -ml-4 -mt-0 p-0"} />
-        <div className="flex flex-row justify-between gap-2 md:gap-12 ">
-          <div className="flex w-1/2 flex-col gap-2">
-            <Label htmlFor="first_name">* First Name:</Label>
-            <Input
-              required
-              id="first_name"
-              name="first_name"
-              placeholder="First Name"
-            />
-          </div>
-          <div className="flex w-1/2 flex-col gap-2">
-            <Label htmlFor="last_name">* Last Name:</Label>
-            <Input
-              required
-              id="last_name"
-              name="last_name"
-              placeholder="Last Name"
-            />
-          </div>
+    <form
+      action={submitForm}
+      className="relative flex w-full flex-col justify-center gap-4 rounded-3xl bg-white p-4 md:p-8"
+    >
+      <FormFieldsHeader className={" -ml-4 -mt-0 p-0"} />
+      <div className="flex flex-row justify-between gap-2 md:gap-12 ">
+        <div className="flex w-1/2 flex-col gap-2">
+          <Label htmlFor="firstName">* First Name:</Label>
+          <Input
+            required
+            id="firstName"
+            name="firstName"
+            placeholder="First Name"
+          />
         </div>
-        <SelectField name="institution" label="Which institution do you go to?">
-          <option selected disabled>
-            Select Insitution
+        <div className="flex w-1/2 flex-col gap-2">
+          <Label htmlFor="lastName">* Last Name:</Label>
+          <Input
+            required
+            id="lastName"
+            name="lastName"
+            placeholder="Last Name"
+          />
+        </div>
+      </div>
+      <SelectField name="institution" label="Which institution do you go to?">
+        <option selected disabled>
+          Select Insitution
+        </option>
+        {institutions.map((institution) => (
+          <option key={institution} value={institution}>
+            {institution}
           </option>
-          {institutions.map((institution) => (
-            <option key={institution} value={institution}>
-              {institution}
-            </option>
-          ))}
-        </SelectField>
-        <SelectField
-          required
-          name="require_food"
-          label="* Do you want provided food at the hackathon?"
-        >
-          <option selected disabled>
-            Select an option
-          </option>
-          <option value={"yes"}>Yes</option>
-          <option value={"no"}>No</option>
-          <option value={"unsure"}>Unsure</option>
-        </SelectField>
-        <Flex direction="column" gap="small">
-          <Label htmlFor="allergies">
-            If you wanted provided food, please indicate any allergies:
-          </Label>
-          <Input id="allergies" name="allergies" placeholder="e.g. peanuts" />
-        </Flex>
-        <FormFields />
-      </form>
-    </>
+        ))}
+      </SelectField>
+      <SelectField
+        required
+        name="meals"
+        label="* Do you want provided food at the hackathon?"
+      >
+        <option selected disabled>
+          Select an option
+        </option>
+        <option value={"yes"}>Yes</option>
+        <option value={"no"}>No</option>
+      </SelectField>
+      <Flex direction="column" gap="small">
+        <Label htmlFor="allergies">
+          If you wanted provided food, please indicate any allergies:
+        </Label>
+        <Input id="allergies" name="allergies" placeholder="e.g. peanuts" />
+      </Flex>
+      <FormFields mutationStatus={userMutation.status} />
+    </form>
   );
 }
