9023b5fa32e0161f22708da2403c2cf0c98f8552
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-16 02:44:49 -0600

    removed unused code

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -9,23 +9,6 @@ const backend = defineBackend({
   data,
 });
 
-backend.auth.resources.cfnResources.cfnUserPool.schema = [
-  {
-    name: "institution",
-    attributeDataType: "String",
-    mutable: true,
-    required: false,
-  },
-  // TODO: Apparently required custom attributes are not supported currently. (CloudFormation Error)
-  // TODO: BTW, you can't delete custom attributes once they are deleted unless you nuke ur sandbox. I changed testUsername to Allergies
-  {
-    name: "testUsername",
-    attributeDataType: "String",
-    mutable: true,
-    required: false,
-  },
-];
-
 const GOOGLE_CLIENT_ID = new secrets.Secret(
   backend.auth.resources.userPool.stack,
   "GOOGLE_CLIENT_ID",
