237d66eea9843af2d7f0c79a471d5f57efd44dd3
Burton Jong <108391733+burtonjong@users.noreply.github.com>
2024-05-28 23:01:05 -0600

    conditionally rendering different types (#29)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,39 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+
+import { UserType, useUser } from "@/components/contexts/UserContext";
 
 const headerContainerStyles =
   "flex flex-row items-center justify-between text-awesomer-purple h-36 bg-white px-8";
 
-const isSignedIn = true;
-const hasTeam = true;
-
 export default function Header() {
+  const user = useUser().currentUser;
+
   return (
     <div className={headerContainerStyles}>
       <div className="flex w-48 font-semibold">
-        {isSignedIn ? (
-          hasTeam && <Link href="/">Join a Team</Link>
+        {user.completedProfile ? (
+          <>
+            {user.type === UserType.Participant ? (
+              <>
+                {user.teamId ? (
+                  <Link href="/participant/profile/team-details">
+                    View Team
+                  </Link>
+                ) : (
+                  <Link href="/participant/register">Join a Team</Link>
+                )}
+              </>
+            ) : user.type === UserType.Admin ? (
+              <Link href="/admin/teams">Admin Dashboard</Link>
+            ) : (
+              <Link href="/judge/dashboard">Judge Dashboard</Link>
+            )}
+          </>
         ) : (
-          <Link href="/">Join Hackathon</Link>
+          <Link href="/login">Join Hackathon</Link>
         )}
       </div>
 
@@ -31,7 +50,7 @@ export default function Header() {
       </div>
 
       <div className="flex w-48 justify-end">
-        {isSignedIn && (
+        {user.completedProfile && (
           <Link href="/participant/profile">
             <Image
               src={
