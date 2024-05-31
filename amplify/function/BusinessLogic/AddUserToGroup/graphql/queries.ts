e61d7b2d03cee1d2907ce39f8d3c1672c01fa351
Burton Jong <burtonjong05@gmail.com>
2024-05-30 21:11:43 -0600

    what am i doing

---
+++ amplify/function/BusinessLogic/AddUserToGroup/graphql/queries.ts
@@ -0,0 +1,109 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+import * as APITypes from "./API";
+type GeneratedQuery<InputType, OutputType> = string & {
+  __generatedQueryInput: InputType;
+  __generatedQueryOutput: OutputType;
+};
+
+export const getTeam = /* GraphQL */ `query GetTeam($id: ID!) {
+  getTeam(id: $id) {
+    approved
+    createdAt
+    id
+    members {
+      nextToken
+      __typename
+    }
+    name
+    owner
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedQuery<APITypes.GetTeamQueryVariables, APITypes.GetTeamQuery>;
+export const getUser = /* GraphQL */ `query GetUser($id: ID!) {
+  getUser(id: $id) {
+    allergies
+    checkedIn
+    createdAt
+    email
+    firstName
+    id
+    institution
+    lastName
+    meals
+    owner
+    role
+    team {
+      approved
+      createdAt
+      id
+      name
+      owner
+      updatedAt
+      __typename
+    }
+    teamId
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedQuery<APITypes.GetUserQueryVariables, APITypes.GetUserQuery>;
+export const listTeams = /* GraphQL */ `query ListTeams(
+  $filter: ModelTeamFilterInput
+  $id: ID
+  $limit: Int
+  $nextToken: String
+  $sortDirection: ModelSortDirection
+) {
+  listTeams(
+    filter: $filter
+    id: $id
+    limit: $limit
+    nextToken: $nextToken
+    sortDirection: $sortDirection
+  ) {
+    items {
+      approved
+      createdAt
+      id
+      name
+      owner
+      updatedAt
+      __typename
+    }
+    nextToken
+    __typename
+  }
+}
+` as GeneratedQuery<APITypes.ListTeamsQueryVariables, APITypes.ListTeamsQuery>;
+export const listUsers = /* GraphQL */ `query ListUsers(
+  $filter: ModelUserFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      allergies
+      checkedIn
+      createdAt
+      email
+      firstName
+      id
+      institution
+      lastName
+      meals
+      owner
+      role
+      teamId
+      updatedAt
+      __typename
+    }
+    nextToken
+    __typename
+  }
+}
+` as GeneratedQuery<APITypes.ListUsersQueryVariables, APITypes.ListUsersQuery>;
