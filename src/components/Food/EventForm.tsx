d5a83995d955fb9cfca23c1ebb43c7e9aa7eea6c
TheSaddestBread <74692833+AllanKoder@users.noreply.github.com>
2024-06-10 18:41:05 -0600

    Install js sha3 to package json (#35)
    Co-authored-by: AnaCoda <anacod@zoho.com>
    Co-authored-by: ideen1 <ibanijamali@gmail.com>

--- src/components/Food/EventForm.tsx
+++ src/components/Food/EventForm.tsx
@@ -5,5 +5,6 @@ import { createFoodEvent } from "@/app/admin/food-tickets/actions";
 
 export default function EventForm() {
   // FIXME: No idea why there is a TypeScript error here, everything works fine though.
+  // @ts-ignore TODO - This will be removed in the task to create food tickets @nataliey16
   return <FoodEventCreateForm onSubmit={createFoodEvent}></FoodEventCreateForm>;
 }
