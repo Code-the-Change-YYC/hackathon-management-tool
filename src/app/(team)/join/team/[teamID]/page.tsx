5ca10e619162ed2af988acd8e6f1e16dd97fbbe4
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-06-23 19:29:22 -0600

    refactor: Update team confirmation page

--- src/app/(team)/join/team/[teamID]/page.tsx
+++ src/app/(team)/join/team/[teamID]/page.tsx
@@ -1,28 +1,5 @@
-import Link from "next/link";
-
-import PurpleButton from "@/components/PurpleButton";
-import CalendarSection from "@/components/teamRegistration/CalendarSection";
-import CountdownWindow from "@/components/teamRegistration/CountdownWindow";
-import TeamInformation from "@/components/teamRegistration/TeamInformation";
-import { Underline } from "@/utils/text-utils";
+import TeamConfirmation from "@/components/teamRegistration/TeamConfirmation";
 
 export default function page({ params }: { params: { teamID: string } }) {
-  return (
-    <>
-      <TeamInformation state={"Joined"} teamID={params.teamID} />
-      <CountdownWindow>
-        <h1 className="text-2xl font-semibold">
-          <Underline noTick>Add to Calendar</Underline>
-        </h1>
-        <hr />
-        <CalendarSection />
-      </CountdownWindow>
-
-      <div className="my-4 flex w-full justify-center text-white sm:justify-end">
-        <Link href="/participant/profile">
-          <PurpleButton className="w-48">Return</PurpleButton>
-        </Link>
-      </div>
-    </>
-  );
+  return <TeamConfirmation state="Joined" teamID={params.teamID} />;
 }
--- src/app/(team)/register/team/[teamID]/page.tsx
+++ src/app/(team)/register/team/[teamID]/page.tsx
@@ -1,28 +1,5 @@
-import Link from "next/link";
-
-import PurpleButton from "@/components/PurpleButton";
-import CalendarSection from "@/components/teamRegistration/CalendarSection";
-import CountdownWindow from "@/components/teamRegistration/CountdownWindow";
-import TeamInformation from "@/components/teamRegistration/TeamInformation";
-import { Underline } from "@/utils/text-utils";
+import TeamConfirmation from "@/components/teamRegistration/TeamConfirmation";
 
 export default function page({ params }: { params: { teamID: string } }) {
-  return (
-    <>
-      <TeamInformation teamID={params.teamID} />
-      <CountdownWindow>
-        <h1 className="text-2xl font-semibold">
-          <Underline noTick>Add to Calendar</Underline>
-        </h1>
-        <hr />
-        <CalendarSection />
-      </CountdownWindow>
-
-      <div className="my-4 flex w-full justify-end text-white">
-        <Link href="/participant/profile">
-          <PurpleButton>Return</PurpleButton>
-        </Link>
-      </div>
-    </>
-  );
+  return <TeamConfirmation state="Registered" teamID={params.teamID} />;
 }
