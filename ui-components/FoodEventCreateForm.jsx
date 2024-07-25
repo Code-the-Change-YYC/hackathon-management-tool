ea85384f95569bee77eab9b87029e053ab9a8cd2
Natalie <natalie.yeung16@gmail.com>
2024-07-25 11:15:43 -0600

    Rename route to create-food-event for clarity

--- ui-components/FoodEventCreateForm.jsx
+++ ui-components/FoodEventCreateForm.jsx
@@ -414,6 +414,8 @@ import { fetchByPath, getOverrideProps, validateField } from "./utils";
 
 /* eslint-disable */
 
+/* eslint-disable */
+
 const client = generateClient();
 export default function FoodEventCreateForm(props) {
   const theme = {
