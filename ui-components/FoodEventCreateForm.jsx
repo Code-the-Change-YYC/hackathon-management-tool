9774bda61a9b51a491a6693b2db2ac65a593ace9
Natalie <natalie.yeung16@gmail.com>
2024-07-25 18:06:09 -0600

    save something

--- ui-components/FoodEventCreateForm.jsx
+++ ui-components/FoodEventCreateForm.jsx
@@ -418,6 +418,8 @@ import { fetchByPath, getOverrideProps, validateField } from "./utils";
 
 /* eslint-disable */
 
+/* eslint-disable */
+
 const client = generateClient();
 export default function FoodEventCreateForm(props) {
   const theme = {
