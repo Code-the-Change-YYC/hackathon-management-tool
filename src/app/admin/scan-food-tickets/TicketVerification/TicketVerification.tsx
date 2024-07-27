3e51b837ef1bb04230ad40f5aa0051bb7121aa7f
Natalie <natalie.yeung16@gmail.com>
2024-07-27 13:21:41 -0600

    Revert accidental changes I made to wrong route

--- src/app/admin/scan-food-tickets/TicketVerification/TicketVerification.tsx
+++ src/app/admin/scan-food-tickets/TicketVerification/TicketVerification.tsx
@@ -5,11 +5,6 @@ import { useState } from "react";
 
 import { verifyFoodTicket } from "./actions";
 
-const HEADER_STYLES = "text-lg";
-const INPUT_STYLES = "rounded-md border w-full p-4 my-1 border-2";
-const SUBMIT_STYLES =
-  "bg-awesomer-purple p-4 w-full my-1 text-white rounded-md hover:bg-[#A689FF]";
-
 export default function TicketVerification() {
   const [inputUserCode, setUserCode] = useState("");
   const [canEatBoolean, setCanEatBoolean] = useState(false);
@@ -30,31 +25,28 @@ export default function TicketVerification() {
 
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center">
-        {canEatBoolean === true && <p>They can Eat!</p>}
-        {eatDescription !== "" && <p>{eatDescription}</p>}
+      {canEatBoolean === true && <p>They can Eat!</p>}
+      {eatDescription !== "" && <p>{eatDescription}</p>}
 
-        <form onSubmit={handleSubmit}>
-          <h1 className={HEADER_STYLES}>Enter food ticket information:</h1>
-          <input
-            type="text"
-            value={inputUserCode}
-            onChange={(e) => setUserCode(e.target.value)}
-            className={INPUT_STYLES}
-            placeholder="Enter ticket code"
-          />
-          <input
-            type="text"
-            value={inputEventIDValue}
-            onChange={(e) => setEventIDValue(e.target.value)}
-            className={INPUT_STYLES}
-            placeholder="Enter Food Event ID"
-          />
-          <button type="submit" className={SUBMIT_STYLES}>
-            Submit
-          </button>
-        </form>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputUserCode}
+          onChange={(e) => setUserCode(e.target.value)}
+          className="border p-2"
+          placeholder="Enter ticket code"
+        />
+        <input
+          type="text"
+          value={inputEventIDValue}
+          onChange={(e) => setEventIDValue(e.target.value)}
+          className="border p-2"
+          placeholder="Enter Food Event ID"
+        />
+        <button type="submit" className="bg-blue-500 p-2 text-white">
+          Submit
+        </button>
+      </form>
     </>
   );
 }
--- src/app/admin/scan-food-tickets/page.tsx
+++ src/app/admin/scan-food-tickets/page.tsx
@@ -1,72 +1,142 @@
 "use client";
 
-import { generateClient } from "aws-amplify/data";
-import React from "react";
-import { useEffect, useState } from "react";
+import { generateClient } from "aws-amplify/api";
+import { useRef, useState } from "react";
+import { toast } from "react-toastify";
 
-import { type Schema } from "@/amplify/data/resource";
-import TicketVerification from "@/app/admin/scan-food-tickets/TicketVerification/TicketVerification";
+import type { Schema } from "@/amplify/data/resource";
+import { useQuery } from "@tanstack/react-query";
+import { Scanner } from "@yudiel/react-qr-scanner";
 
-type FoodEvent = Schema["FoodEvent"]["type"];
+import { verifyFoodTicket } from "./TicketVerification/actions";
 
-const ScanFoodTickets = () => {
+const QR_SCANNER_SECTION_STYLES =
+  "overflow-x-hidden bg-medium-grey text-blackish";
+const QR_SCANNER_CONTENT_STYLES =
+  "container mx-auto flex items-center justify-center p-4";
+const QR_SCANNER_CONTAINER_STYLES =
+  "w-full max-w-[500px] rounded-lg bg-white p-6 drop-shadow-md md:w-auto";
+
+const SELECT_FOOD_EVENT_STYLES =
+  "mb-4 w-full rounded-md border border-blackish p-2 focus:border-2 focus:border-awesomer-purple focus:outline-none";
+const QR_READER_CONTAINER_STYLES =
+  "border-awesome-purple bg-awesome-purple rounded-md border-2";
+const MANUAL_SCAN_BUTTON_STYLES =
+  "hover:bg-lilac-purple mx-auto mt-6 block rounded-xl bg-awesomer-purple px-4 py-2 font-medium text-white";
+
+const AdminFoodTickets = () => {
+  const [scanResult, setScanResult] = useState("");
+  const [canEatBoolean, setCanEatBoolean] = useState(false);
+  const [eatDescription, setEatDescription] = useState("");
+  const [inputEventIDValue, setEventIDValue] = useState("");
+  const isVerifying = useRef(false);
   const client = generateClient<Schema>();
-  const [foodData, setFoodData] = useState<FoodEvent[]>();
 
-  useEffect(() => {
-    async function fetchData() {
+  // In your component
+  const {
+    data: foodEvents,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["foodEvents"],
+    queryFn: async () => {
       const { data, errors } = await client.models.FoodEvent.list();
-      if (!errors) {
-        setFoodData(data); // Update state with fetched data
-      } else {
-        console.error(errors); // Log errors if any
+      if (errors) {
+        throw errors;
       }
+      return data;
+    },
+    initialData: [],
+    initialDataUpdatedAt: 0,
+  });
+
+  const verifyTicket = async (scanCode: string) => {
+    if (scanCode && inputEventIDValue && !isVerifying.current) {
+      isVerifying.current = true;
+      toast.info("Verifying ticket...");
+      const { canEat, description } = await verifyFoodTicket(
+        scanCode,
+        inputEventIDValue,
+      );
+      setCanEatBoolean(canEat);
+      setEatDescription(description);
+      toast.dismiss();
+      if (canEat) {
+        toast.success("Ticket verified successfully!");
+      } else {
+        toast.error("Ticket verification failed!");
+      }
+      isVerifying.current = false;
     }
-    fetchData();
-  }, []);
+  };
+
+  const handleEventChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setEventIDValue(event.target.value);
+  };
+
+  const handleManualScan = () => {
+    verifyTicket(scanResult);
+  };
+
   return (
-    <>
-      <div className="h-full bg-[#d9d9d9]">
-        <div className="m-6 rounded-md bg-white">
-          <TicketVerification />
-          <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
-            {foodData &&
-              foodData.map((event) => (
-                <div
-                  key={event.id}
-                  className="w-auto rounded-lg bg-white p-6 shadow-md"
-                >
-                  <h3 className="text-lg font-semibold">{event.name}</h3>
-                  <p className="text-sm text-gray-600">{event.description}</p>
-                  <p className="text-sm">
-                    <strong>ID:</strong> {event?.id}
-                  </p>
-                  <p className="text-sm">
-                    <strong>Start:</strong>{" "}
-                    {event.start ? new Date(event.start).toLocaleString() : ""}
-                  </p>
-                  <p className="text-sm">
-                    <strong>End:</strong>
-                    {event.end ? new Date(event.end).toLocaleString() : ""}
-                  </p>
-                  <p className="text-sm">
-                    <strong>Groups:</strong> {event.totalGroupCount}
-                  </p>
-                  <p className="text-sm">
-                    <strong>Created At:</strong>{" "}
-                    {new Date(event.createdAt).toLocaleString()}
-                  </p>
-                  <p className="text-sm">
-                    <strong>Updated At:</strong>{" "}
-                    {new Date(event.updatedAt).toLocaleString()}
-                  </p>
-                </div>
-              ))}
+    <div className={QR_SCANNER_SECTION_STYLES}>
+      <div className={QR_SCANNER_CONTENT_STYLES}>
+        <div className={QR_SCANNER_CONTAINER_STYLES}>
+          <h1 className="mb-4 text-2xl font-bold">Scan Food Ticket</h1>
+          <div className="mb-2">
+            {canEatBoolean && <p>They can Eat!</p>}
+            {eatDescription && <p>{eatDescription}</p>}
           </div>
+          <div className="relative">
+            <select
+              name="foodEvent"
+              id="foodEvent"
+              className={SELECT_FOOD_EVENT_STYLES}
+              onChange={handleEventChange}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <option>Loading food events...</option>
+              ) : error ? (
+                <option>Error loading food events</option>
+              ) : (
+                <>
+                  <option value="">Select a food event</option>
+                  {foodEvents.map((event) => (
+                    <option key={event.id} value={event.id}>
+                      <span>{event.name}: </span>
+                      <span>
+                        {new Date(event.start).toLocaleString()} -{" "}
+                        {new Date(event.end).toLocaleString()}
+                      </span>
+                    </option>
+                  ))}
+                </>
+              )}
+            </select>
+            <div className={QR_READER_CONTAINER_STYLES}>
+              <Scanner
+                scanDelay={50}
+                onScan={(result) => {
+                  if (result) {
+                    setScanResult(result[0].rawValue);
+                    verifyTicket(result[0].rawValue);
+                  }
+                }}
+                constraints={{ facingMode: "environment" }}
+              />
+            </div>
+          </div>
+          <button
+            className={MANUAL_SCAN_BUTTON_STYLES}
+            onClick={handleManualScan}
+          >
+            Manual Scan
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default ScanFoodTickets;
+export default AdminFoodTickets;
