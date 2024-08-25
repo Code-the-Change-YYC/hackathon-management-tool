f5bc7ae7a0857556641f69e4f1b4c07e39fa704a
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-08-25 15:19:00 -0600

    added contentful

---
+++ src/app/contentfulTypes.ts
@@ -0,0 +1,22 @@
+import type { Asset, BaseEntry } from "contentful";
+
+export interface Alumni extends BaseEntry {
+  fields: {
+    fullName: string;
+    linkedin: string;
+    orderNumber: number;
+    position: string;
+    previousRole: string;
+    testimonial: string;
+    profile: Asset;
+  };
+}
+export interface Judge extends BaseEntry {
+  fields: {
+    judgeImg: Asset;
+    judgeName: string;
+    judgeCompany: string;
+    orderNumber?: number;
+    position?: string;
+  };
+}
