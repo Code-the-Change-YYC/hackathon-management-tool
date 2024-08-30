6155731e5fc10c2180267c0a4617736212268e28
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-08-30 15:54:45 -0600

    added contentful (#93)

---
+++ src/app/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { createClient } from "contentful";
+
+const contentfulClient = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID ?? "",
+  accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN ?? "",
+});
+export type ContentType =
+  | "alumni"
+  | "event"
+  | "executive"
+  | "pastEvents"
+  | "timeline"
+  | "hackathonJudge"
+  | "upcomingEvents";
+
+export async function fetchContent(contentId: ContentType) {
+  try {
+    const res = await contentfulClient.getEntries({ content_type: contentId });
+    const data = res.items;
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to fetch content" + (err as Error).message);
+  }
+}
