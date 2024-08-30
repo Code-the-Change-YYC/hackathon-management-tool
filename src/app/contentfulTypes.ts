ff4e7f6b79351ab0929901121ada0d04a62855bd
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-08-30 16:07:13 -0600

    Hmt 12 contentful landing page hero section (#96)

--- src/app/contentfulTypes.ts
+++ src/app/contentfulTypes.ts
@@ -20,3 +20,13 @@ export interface Judge extends BaseEntry {
     position?: string;
   };
 }
+export interface HackathonDetails extends BaseEntry {
+  fields: {
+    eventName: string;
+    eventBlurb: string;
+    eventDate: string;
+    locationName: string;
+    locationImage: Asset;
+    prizeAmount: number;
+  };
+}
