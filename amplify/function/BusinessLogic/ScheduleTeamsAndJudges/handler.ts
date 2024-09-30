65aec55d383d55d3c5ec7c6506f75a3cb7b9dc05
ideen1 <ibanijamali@gmail.com>
2024-09-30 16:07:25 -0600

    Final changes

--- amplify/function/BusinessLogic/ScheduleTeamsAndJudges/handler.ts
+++ amplify/function/BusinessLogic/ScheduleTeamsAndJudges/handler.ts
@@ -2,19 +2,15 @@ import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 
 import type { Schema } from "../../../data/resource";
-import { deleteRoom } from "../AddUserToGroup/graphql/mutations";
+import { createTeamRoom, updateUser } from "./graphql/mutations";
 import {
-  createRoom,
-  createTeamRoom,
-  deleteTeamRoom,
-  updateUser,
-} from "./graphql/mutations";
-import {
-  listRooms,
-  listTeamRooms,
-  listTeams,
-  listUsers,
-} from "./graphql/queries";
+  createRooms,
+  deleteRooms,
+  deleteTeamRooms,
+  fetchApprovedTeams,
+  fetchJudges,
+  validateInput,
+} from "./helpers";
 
 Amplify.configure(
   {
@@ -52,7 +48,7 @@ const InternalError = JSON.stringify({
   headers: { "Content-Type": "application/json" },
 });
 
-const client = generateClient<Schema>({
+export const client = generateClient<Schema>({
   authMode: "iam",
 });
 
@@ -136,12 +132,13 @@ export const handler: Schema["ScheduleTeamsAndJudges"]["functionHandler"] =
     // Create all the team rooms
     await Promise.all(createTeamRoomRequests)
       .then((res) => {
-        for (let i = 0; i < res.length; i++) {
-          if (res[i].errors) {
-            console.log(res[i].errors);
-            throw new Error();
+        res.forEach((result) => {
+          if (result.errors) {
+            console.log(result.errors);
+            throw new Error("Unable to create teamRooms");
           }
-        }
+        });
+
         return "Created Rooms";
       })
       .catch((error) => {
@@ -173,7 +170,7 @@ export const handler: Schema["ScheduleTeamsAndJudges"]["functionHandler"] =
         for (let i = 0; i < res.length; i++) {
           if (res[i].errors) {
             console.log(res[i].errors);
-            throw new Error();
+            throw new Error("Unable to update judges with roomIds");
           }
         }
         return "Updates Judges";
@@ -189,162 +186,3 @@ export const handler: Schema["ScheduleTeamsAndJudges"]["functionHandler"] =
       headers: { "Content-Type": "application/json" },
     };
   };
-
-async function deleteRooms() {
-  const response = await client.graphql({ query: listRooms });
-  if (response.errors) {
-    throw new Error("Error fetching existing rooms");
-  }
-  const roomsToDelete = response.data.listRooms.items;
-
-  // Perform deletion using GraphQL
-  const roomDeletionResult = await Promise.all(
-    roomsToDelete.map((room) => {
-      return client.graphql({
-        query: deleteRoom,
-        variables: { input: { id: room.id } },
-      });
-    }),
-  );
-
-  // Check for errors in the deletion process
-  for (let i = 0; i < roomDeletionResult.length; i++) {
-    const result = roomDeletionResult[i];
-    if (result.errors) {
-      throw new Error(`Error deleting room with Id: ${roomsToDelete[i].id}`);
-    }
-  }
-
-  return Promise.resolve("All rooms deleted successfully");
-}
-
-async function deleteTeamRooms() {
-  const response = await client.graphql({ query: listTeamRooms });
-  if (response.errors) {
-    throw new Error("Error fetching existing team rooms");
-  }
-  const teamRoomsToDelete = response.data.listTeamRooms.items;
-
-  // Perform deletion using GraphQL
-  const teamRoomDeletionResult = await Promise.all(
-    teamRoomsToDelete.map((room) => {
-      return client.graphql({
-        query: deleteTeamRoom,
-        variables: { input: { id: room.id } },
-      });
-    }),
-  );
-
-  // Check for errors in the deletion process
-  for (let i = 0; i < teamRoomDeletionResult.length; i++) {
-    const result = teamRoomDeletionResult[i];
-    if (result.errors) {
-      throw new Error(
-        `Error deleting teamRoom with Id: ${teamRoomsToDelete[i].id}`,
-      );
-    }
-  }
-
-  return Promise.resolve("All teamRooms deleted successfully");
-}
-
-function validateInput(
-  judgingSessionsPerTeam: number,
-  numOfJudgingRooms: number,
-  numTeams: number,
-) {
-  if (judgingSessionsPerTeam > numOfJudgingRooms) {
-    throw new Error(
-      JSON.stringify({
-        body: {
-          value:
-            "Cannot have more judging sessions than there are judging rooms",
-        },
-        statusCode: 400,
-        headers: { "Content-Type": "application/json" },
-      }),
-    );
-  }
-
-  if (numOfJudgingRooms > numTeams && judgingSessionsPerTeam > 1) {
-    throw new Error(
-      JSON.stringify({
-        body: {
-          value: "Not enough teams and too many judges",
-        },
-        statusCode: 400,
-        headers: { "Content-Type": "application/json" },
-      }),
-    );
-  }
-}
-
-async function fetchApprovedTeams() {
-  const teamsResponse = await client.graphql({
-    query: listTeams,
-    variables: {
-      filter: {
-        approved: { eq: true },
-      },
-    },
-  });
-  if (teamsResponse.errors) {
-    console.log("error fetching approved teams");
-    throw new Error("error fetching approved teams");
-  }
-  return teamsResponse.data.listTeams.items;
-}
-
-async function createRooms(numRooms: number) {
-  const createRoomRequests = Array.from({ length: numRooms }, (_, i) => {
-    return client.graphql({
-      query: createRoom,
-      variables: {
-        input: {
-          name: `Room ${i + 1}`,
-        },
-      },
-    });
-  });
-  const createRoomsResult = await Promise.all(createRoomRequests).catch(
-    (error) => {
-      console.log(error);
-      throw new Error("unable to create rooms");
-    },
-  );
-
-  let roomIds: string[] = [];
-  for (let i = 0; i < createRoomsResult.length; i++) {
-    const result = createRoomsResult[i];
-    if (result.errors) {
-      console.log(result.errors);
-      throw new Error("unable to create rooms");
-    }
-    roomIds.push(result.data.createRoom.id);
-  }
-
-  return roomIds;
-}
-
-async function fetchJudges() {
-  const judgesResponse = await client
-    .graphql({
-      query: listUsers,
-      variables: {
-        filter: {
-          role: { eq: "JUDGE" },
-        },
-      },
-    })
-    .catch((error) => {
-      console.log(error);
-      throw new Error("Unable to fetch judges");
-    });
-
-  if (judgesResponse.errors) {
-    console.log(judgesResponse.errors);
-    throw new Error("Unable to fetch judges");
-  }
-
-  return judgesResponse.data.listUsers.items;
-}
