e5785978e746cc964cd7a67a68ce87ee88b4634b
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-10-22 21:27:14 -0600

    fix join team bug (#121)

--- amplify/function/BusinessLogic/AssignUsersToTeams/handler.ts
+++ amplify/function/BusinessLogic/AssignUsersToTeams/handler.ts
@@ -3,9 +3,8 @@ import { generateClient } from "aws-amplify/data";
 import type { AppSyncResolverHandler } from "aws-lambda";
 
 import type { Schema } from "../../../data/resource";
-import type { ModelUserConnection } from "./graphql/API";
+import { data } from "../_amplify_model/amplify_outputs.json";
 import { updateUser } from "./graphql/mutations";
-import { getTeam, getUser } from "./graphql/queries";
 
 const MAX_TEAM_MEMBERS = 6;
 Amplify.configure(
@@ -15,6 +14,7 @@ Amplify.configure(
         endpoint: process.env.AMPLIFY_DATA_GRAPHQL_ENDPOINT as string,
         region: process.env.AWS_REGION,
         defaultAuthMode: "iam",
+        modelIntrospection: data.model_introspection as any,
       },
     },
   },
@@ -52,96 +52,78 @@ export const handler: AppSyncResolverHandler<
   ResolverArgs,
   ResolverResult
 > = async (event, _) => {
-  try {
-    const user = (
-      await client.graphql({
-        query: getUser,
-        variables: {
-          id: event.arguments.userId,
-        },
-      })
-    ).data.getUser;
-    const team = (
-      await client.graphql({
-        query: getTeam,
-        variables: {
-          id: event.arguments.teamId,
-        },
-      })
-    ).data.getTeam;
-
-    console.log(user);
-    console.log(team);
-    if (team == null) {
-      throw new Error(
-        JSON.stringify({
-          body: { value: "Error: Team does not exist" },
-          statusCode: 404,
-          headers: { "Content-Type": "application/json" },
-        }),
-      );
-    }
-
-    if (user == null) {
-      throw new Error(
-        JSON.stringify({
-          body: { value: "Error: User does not exist" },
-          statusCode: 404,
-          headers: { "Content-Type": "application/json" },
-        }),
-      );
-    }
-
-    if (user.team) {
-      throw new Error(
-        JSON.stringify({
-          body: { value: "Error: User is already part of a team" },
-          statusCode: 400,
-          headers: { "Content-Type": "application/json" },
-        }),
-      );
-    }
-    const members = (team.members as ModelUserConnection).items;
-
-    if (members.length >= MAX_TEAM_MEMBERS) {
-      throw new Error(
-        JSON.stringify({
-          body: { value: "Error: Team is full" },
-          statusCode: 400,
-          headers: { "Content-Type": "application/json" },
-        }),
-      );
-    }
-
-    const result = await client.graphql({
-      query: updateUser,
-      variables: {
-        input: {
-          id: user.id,
-          teamId: team.id,
-        },
-      },
-    });
-
-    if (!result.errors) {
-      return {
-        body: { value: `Success` },
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-      };
-    } else {
-      throw new Error(
-        JSON.stringify({
-          body: { value: `Error while updating database` },
-          statusCode: 500,
-          headers: { "Content-Type": "application/json" },
-        }),
-      );
-    }
-  } catch {
+  const user = (
+    await client.models.User.get({
+      id: event.arguments.userId,
+    })
+  ).data;
+  const team = (
+    await client.models.Team.get({
+      id: event.arguments.teamId,
+    })
+  ).data;
+  if (team == null) {
     throw new Error(
       JSON.stringify({
-        body: { value: `Unhandled Internal Server Error` },
+        body: { value: "Error: Team does not exist" },
+        statusCode: 404,
+        headers: { "Content-Type": "application/json" },
+      }),
+    );
+  }
+
+  if (user == null) {
+    throw new Error(
+      JSON.stringify({
+        body: { value: "Error: User does not exist" },
+        statusCode: 404,
+        headers: { "Content-Type": "application/json" },
+      }),
+    );
+  }
+
+  if ((await user.team()).data) {
+    throw new Error(
+      JSON.stringify({
+        body: { value: "Error: User is already part of a team" },
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+      }),
+    );
+  }
+  const members = (await team.members()).data;
+
+  if (members.length >= MAX_TEAM_MEMBERS) {
+    throw new Error(
+      JSON.stringify({
+        body: { value: "Error: Team is full" },
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+      }),
+    );
+  }
+
+  const result = await client.graphql({
+    query: updateUser,
+    variables: {
+      input: {
+        id: user.id,
+        teamId: team.id,
+      },
+    },
+  });
+
+  if (!result.errors) {
+    return {
+      body: { value: `Success` },
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+    };
+  } else {
+    console.error(result.errors);
+    throw new Error(
+      JSON.stringify({
+        body: { value: `Error while updating database` },
         statusCode: 500,
         headers: { "Content-Type": "application/json" },
       }),
