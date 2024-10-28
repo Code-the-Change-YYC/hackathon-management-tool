f7fc92b38b4cbbaec0ff5c1104a925ce1d2f7564
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-10-27 22:15:21 -0600

    Fix timer in team complete (#134)

--- src/components/teamRegistration/CountdownWindow.tsx
+++ src/components/teamRegistration/CountdownWindow.tsx
@@ -1,95 +1,16 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
-import CountdownTimer from "@/components/LandingPage/CountdownTimer";
+import HackathonClock from "../HackathonClock";
 
 export default function CountdownWindow({
+  hackathonInformation,
   children,
 }: {
+  hackathonInformation: { eventName: string; eventDate: Date };
   children?: React.ReactNode;
 }) {
-  const formatDate = (unixTimestamp: number): string => {
-    const date = new Date(unixTimestamp * 1000);
-    return date.toLocaleDateString();
-  };
-  const eventDate = 1731394799; //UNIX Time stamp: Nov 10, 2024
-  const eventDateFormatted = formatDate(eventDate);
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [hackathonTime, setHackathonTime] = useState(false);
-  useEffect(() => {
-    const target = new Date(eventDateFormatted);
-
-    const interval = setInterval(() => {
-      const now = new Date();
-      const difference = target.getTime() - now.getTime();
-
-      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-      setDays(d);
-
-      const h = Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-      );
-      setHours(h);
-
-      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      setMinutes(m);
-
-      const s = Math.floor((difference % (1000 * 60)) / 1000);
-      setSeconds(s);
-      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
-        setHackathonTime(true);
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-  const HackathonClock = () => {
-    return (
-      <>
-        <h2 className="text-center text-3xl font-bold text-black">
-          Hack the Change 2024 begins...
-        </h2>
-        <div className="flex flex-row gap-4">
-          <CountdownTimer name="Days" value={days} />
-          <CountdownTimer name="Hours" value={hours} />
-          <CountdownTimer name="Minutes" value={minutes} />
-          <CountdownTimer name="Seconds" value={seconds} />
-        </div>
-        <div className="flex flex-row items-center gap-4 sm:items-end">
-          <CountdownTimer value={"Nov"} />
-          <CountdownTimer value={11} />
-          <div className=" text-4xl text-awesome-purple">to</div>
-          <CountdownTimer value={12} />
-        </div>
-      </>
-    );
-  };
-  const HackathonStart = () => (
-    <h1
-      className="flex-wrap text-4xl font-black text-awesomer-purple drop-shadow-lg md:text-center md:text-5xl"
-      style={{
-        textShadow: `
-                -2px -2px 0 #fff, 
-                2px -2px 0 #fff, 
-                -2px 2px 0 #fff, 
-                2px 2px 0 #fff,
-                -2px -2px 0 #fff,
-                2px -2px 0 #fff,
-                -2px 2px 0 #fff,
-                2px 2px 0 #fff
-              `,
-      }}
-    >
-      Hack the Change 2024
-      <br />
-      has begun!
-    </h1>
-  );
+  const { eventName, eventDate } = hackathonInformation;
 
   return (
     <>
@@ -109,7 +30,7 @@ export default function CountdownWindow({
             Your registration is complete!
           </h1>
         </div>
-        {hackathonTime ? <HackathonStart /> : <HackathonClock />}
+        <HackathonClock eventDate={eventDate} eventName={eventName} />
         <p className="px-4 text-center text-lg font-semibold text-black">
           Join us for the two-day for-charity hackathon hosted on November 11th
           to 12th by{" "}
