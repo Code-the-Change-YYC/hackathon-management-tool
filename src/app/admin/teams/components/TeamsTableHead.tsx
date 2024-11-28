93c44ef4a4c7b9bb9854e4cf506530867d283f6c
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-11-27 22:03:50 -0700

    feat:refactor teams table with tanstackTable

---
+++ src/app/admin/teams/components/TeamsTableHead.tsx
@@ -0,0 +1,52 @@
+import { memo } from "react";
+
+import type { HeaderGroup } from "@tanstack/react-table";
+import { flexRender } from "@tanstack/react-table";
+
+import type { Team } from "../tanstackTableSetup";
+
+const TeamsTableHead = ({ table }: { table: HeaderGroup<Team>[] }) => {
+  return (
+    <thead className=" bg-awesome-purple text-white">
+      {table.map((headerGroup) => (
+        <tr key={headerGroup.id}>
+          {headerGroup.headers.map((header) => (
+            <th className="p-4 text-start font-light" key={header.id}>
+              {header.isPlaceholder ? null : (
+                <div
+                  className={
+                    header.column.getCanSort()
+                      ? "flex cursor-pointer select-none gap-2"
+                      : ""
+                  }
+                  onClick={header.column.getToggleSortingHandler()}
+                  title={
+                    header.column.getCanSort()
+                      ? header.column.getNextSortingOrder() === "asc"
+                        ? "Sort ascending"
+                        : header.column.getNextSortingOrder() === "desc"
+                          ? "Sort descending"
+                          : "Clear sort"
+                      : undefined
+                  }
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext(),
+                  )}
+                  {
+                    {
+                      asc: " 🔼",
+                      desc: " 🔽",
+                    }[header.column.getIsSorted() as string]
+                  }
+                </div>
+              )}
+            </th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  );
+};
+export default memo(TeamsTableHead);
