59172ee1e7ba8f4f88cbe78c0bad2e30cabebaee
Hooriya Khan <riyeur@hotmail.com>
2024-12-04 03:38:29 -0700

    Fixed misaligned judges profiles

--- src/components/LandingPage/JudgeShowcase.tsx
+++ src/components/LandingPage/JudgeShowcase.tsx
@@ -19,9 +19,9 @@ export default async function JudgeShowcase() {
       </div>
       <div className="flex w-full flex-row flex-wrap">
         {judges.map((judge, index) => (
-          <div className="flex w-1/2 justify-center sm:w-1/3" key={index}>
-            <div className="flex w-4/5 flex-row gap-2 py-4">
-              <div className="relative size-16 min-w-16 overflow-hidden rounded-full sm:size-24 sm:min-w-24">
+          <div className="flex w-1/3 justify-center py-5 pl-7" key={index}>
+            <div className="flex flex-row items-center gap-4">
+              <div className="relative size-16 overflow-hidden rounded-full sm:size-24">
                 <Image
                   src={
                     judge.fields.judgeImg.fields.file?.url
@@ -34,10 +34,10 @@ export default async function JudgeShowcase() {
                 />
               </div>
               <div className="flex flex-col justify-center">
-                <p className="text-xs font-extrabold text-awesome-purple sm:text-base">
+                <p className="w-36 text-xs font-extrabold text-awesome-purple sm:text-base">
                   {judge.fields.judgeName}
                 </p>
-                <p className="text-xs sm:text-base">
+                <p className="w-36 text-xs sm:text-base">
                   {judge.fields.judgeCompany}
                 </p>
               </div>
