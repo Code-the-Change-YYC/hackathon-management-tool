c9fde4e7f7903b76bb22715b2b4951e50e701f97
Hooriya <121422181+riyeur@users.noreply.github.com>
2024-12-07 14:57:15 -0700

    161 judges profiles aligned (#168)
    Co-authored-by: Justin Pham <113923596+justin-phxm@users.noreply.github.com>
    Co-authored-by: Burton Jong <108391733+burtonjong@users.noreply.github.com>

--- src/components/LandingPage/JudgeShowcase.tsx
+++ src/components/LandingPage/JudgeShowcase.tsx
@@ -19,12 +19,9 @@ export default async function JudgeShowcase() {
       </div>
       <div className="flex w-full flex-row flex-wrap">
         {judges.map((judge, index) => (
-          <div
-            className="flex w-1/2 flex-row py-10 sm:w-1/3 sm:justify-center"
-            key={index}
-          >
-            <div className="flex flex-row gap-2 sm:gap-3">
-              <div className="relative size-16 min-w-16 overflow-hidden rounded-full sm:size-24 sm:min-w-24">
+          <div className="flex w-1/3 justify-center py-5 pl-7" key={index}>
+            <div className="flex flex-row items-center gap-4">
+              <div className="relative size-16 overflow-hidden rounded-full sm:size-24">
                 <Image
                   src={
                     judge.fields.judgeImg.fields.file?.url
@@ -37,10 +34,10 @@ export default async function JudgeShowcase() {
                 />
               </div>
               <div className="flex flex-col justify-center">
-                <p className="text-xs font-extrabold text-awesome-purple sm:text-base">
+                <p className="w-36 text-xs font-extrabold text-awesome-purple sm:text-base">
                   {judge.fields.judgeName}
                 </p>
-                <p className="text-xs sm:text-base">
+                <p className="w-36 text-xs sm:text-base">
                   {judge.fields.judgeCompany}
                 </p>
               </div>
