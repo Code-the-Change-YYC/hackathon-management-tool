3405b860d30b0f3e0a36a49511ae809c604b000d
Oluwafisayo Adabs <116133322+fisayoadabs@users.noreply.github.com>
2025-01-30 08:53:46 -0700

    Added a preload function on the page before calling the UserProfile.tsx (#172)
    Co-authored-by: burtonjong <burtonjong05@gmail.com>
    Co-authored-by: Burton Jong <108391733+burtonjong@users.noreply.github.com>

--- src/app/participant/profile/page.tsx
+++ src/app/participant/profile/page.tsx
@@ -1,4 +1,13 @@
-import UserProfile from "@/components/UserProfile/UserProfile";
+import dynamic from "next/dynamic";
+
+// Dynamically import UserProfile with preloading
+const UserProfile = dynamic(
+  () => import("@/components/UserProfile/UserProfile"),
+  {
+    ssr: false,
+    loading: () => <p>Loading user profile...</p>,
+  },
+);
 
 export default function Profile() {
   return <UserProfile />;
