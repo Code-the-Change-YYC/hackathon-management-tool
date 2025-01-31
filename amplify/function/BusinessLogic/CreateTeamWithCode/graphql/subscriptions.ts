0828a0096c70933308888494f33b9d5e5f831d84
E J <emiko.emiko@ucalgary.ca>
2025-01-31 15:44:13 -0700

    changes

--- amplify/function/BusinessLogic/CreateTeamWithCode/graphql/subscriptions.ts
+++ amplify/function/BusinessLogic/CreateTeamWithCode/graphql/subscriptions.ts
@@ -1,14 +1,16 @@
 /* tslint:disable */
+
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
-
 import * as APITypes from "./API";
+
 type GeneratedSubscription<InputType, OutputType> = string & {
   __generatedSubscriptionInput: InputType;
   __generatedSubscriptionOutput: OutputType;
 };
 
-export const onCreateFoodEvent = /* GraphQL */ `subscription OnCreateFoodEvent($filter: ModelSubscriptionFoodEventFilterInput) {
+export const onCreateFoodEvent =
+  /* GraphQL */ `subscription OnCreateFoodEvent($filter: ModelSubscriptionFoodEventFilterInput) {
   onCreateFoodEvent(filter: $filter) {
     attended {
       nextToken
@@ -26,10 +28,11 @@ export const onCreateFoodEvent = /* GraphQL */ `subscription OnCreateFoodEvent($
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnCreateFoodEventSubscriptionVariables,
-  APITypes.OnCreateFoodEventSubscription
->;
-export const onCreateHackathon = /* GraphQL */ `subscription OnCreateHackathon($filter: ModelSubscriptionHackathonFilterInput) {
+    APITypes.OnCreateFoodEventSubscriptionVariables,
+    APITypes.OnCreateFoodEventSubscription
+  >;
+export const onCreateHackathon =
+  /* GraphQL */ `subscription OnCreateHackathon($filter: ModelSubscriptionHackathonFilterInput) {
   onCreateHackathon(filter: $filter) {
     createdAt
     endDate
@@ -56,10 +59,11 @@ export const onCreateHackathon = /* GraphQL */ `subscription OnCreateHackathon($
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnCreateHackathonSubscriptionVariables,
-  APITypes.OnCreateHackathonSubscription
->;
-export const onCreateRoom = /* GraphQL */ `subscription OnCreateRoom($filter: ModelSubscriptionRoomFilterInput) {
+    APITypes.OnCreateHackathonSubscriptionVariables,
+    APITypes.OnCreateHackathonSubscription
+  >;
+export const onCreateRoom =
+  /* GraphQL */ `subscription OnCreateRoom($filter: ModelSubscriptionRoomFilterInput) {
   onCreateRoom(filter: $filter) {
     createdAt
     id
@@ -77,10 +81,11 @@ export const onCreateRoom = /* GraphQL */ `subscription OnCreateRoom($filter: Mo
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnCreateRoomSubscriptionVariables,
-  APITypes.OnCreateRoomSubscription
->;
-export const onCreateScore = /* GraphQL */ `subscription OnCreateScore($filter: ModelSubscriptionScoreFilterInput) {
+    APITypes.OnCreateRoomSubscriptionVariables,
+    APITypes.OnCreateRoomSubscription
+  >;
+export const onCreateScore =
+  /* GraphQL */ `subscription OnCreateScore($filter: ModelSubscriptionScoreFilterInput) {
   onCreateScore(filter: $filter) {
     createdAt
     hackathon {
@@ -127,10 +132,11 @@ export const onCreateScore = /* GraphQL */ `subscription OnCreateScore($filter: 
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnCreateScoreSubscriptionVariables,
-  APITypes.OnCreateScoreSubscription
->;
-export const onCreateTeam = /* GraphQL */ `subscription OnCreateTeam($filter: ModelSubscriptionTeamFilterInput) {
+    APITypes.OnCreateScoreSubscriptionVariables,
+    APITypes.OnCreateScoreSubscription
+  >;
+export const onCreateTeam =
+  /* GraphQL */ `subscription OnCreateTeam($filter: ModelSubscriptionTeamFilterInput) {
   onCreateTeam(filter: $filter) {
     approved
     createdAt
@@ -153,10 +159,11 @@ export const onCreateTeam = /* GraphQL */ `subscription OnCreateTeam($filter: Mo
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnCreateTeamSubscriptionVariables,
-  APITypes.OnCreateTeamSubscription
->;
-export const onCreateTeamRoom = /* GraphQL */ `subscription OnCreateTeamRoom($filter: ModelSubscriptionTeamRoomFilterInput) {
+    APITypes.OnCreateTeamSubscriptionVariables,
+    APITypes.OnCreateTeamSubscription
+  >;
+export const onCreateTeamRoom =
+  /* GraphQL */ `subscription OnCreateTeamRoom($filter: ModelSubscriptionTeamRoomFilterInput) {
   onCreateTeamRoom(filter: $filter) {
     createdAt
     id
@@ -184,9 +191,9 @@ export const onCreateTeamRoom = /* GraphQL */ `subscription OnCreateTeamRoom($fi
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnCreateTeamRoomSubscriptionVariables,
-  APITypes.OnCreateTeamRoomSubscription
->;
+    APITypes.OnCreateTeamRoomSubscriptionVariables,
+    APITypes.OnCreateTeamRoomSubscription
+  >;
 export const onCreateUser = /* GraphQL */ `subscription OnCreateUser(
   $filter: ModelSubscriptionUserFilterInput
   $profileOwner: String
@@ -237,7 +244,8 @@ export const onCreateUser = /* GraphQL */ `subscription OnCreateUser(
   APITypes.OnCreateUserSubscriptionVariables,
   APITypes.OnCreateUserSubscription
 >;
-export const onCreateUserFoodEventAttendance = /* GraphQL */ `subscription OnCreateUserFoodEventAttendance(
+export const onCreateUserFoodEventAttendance =
+  /* GraphQL */ `subscription OnCreateUserFoodEventAttendance(
   $filter: ModelSubscriptionUserFoodEventAttendanceFilterInput
 ) {
   onCreateUserFoodEventAttendance(filter: $filter) {
@@ -279,10 +287,11 @@ export const onCreateUserFoodEventAttendance = /* GraphQL */ `subscription OnCre
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnCreateUserFoodEventAttendanceSubscriptionVariables,
-  APITypes.OnCreateUserFoodEventAttendanceSubscription
->;
-export const onDeleteFoodEvent = /* GraphQL */ `subscription OnDeleteFoodEvent($filter: ModelSubscriptionFoodEventFilterInput) {
+    APITypes.OnCreateUserFoodEventAttendanceSubscriptionVariables,
+    APITypes.OnCreateUserFoodEventAttendanceSubscription
+  >;
+export const onDeleteFoodEvent =
+  /* GraphQL */ `subscription OnDeleteFoodEvent($filter: ModelSubscriptionFoodEventFilterInput) {
   onDeleteFoodEvent(filter: $filter) {
     attended {
       nextToken
@@ -300,10 +309,11 @@ export const onDeleteFoodEvent = /* GraphQL */ `subscription OnDeleteFoodEvent($
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnDeleteFoodEventSubscriptionVariables,
-  APITypes.OnDeleteFoodEventSubscription
->;
-export const onDeleteHackathon = /* GraphQL */ `subscription OnDeleteHackathon($filter: ModelSubscriptionHackathonFilterInput) {
+    APITypes.OnDeleteFoodEventSubscriptionVariables,
+    APITypes.OnDeleteFoodEventSubscription
+  >;
+export const onDeleteHackathon =
+  /* GraphQL */ `subscription OnDeleteHackathon($filter: ModelSubscriptionHackathonFilterInput) {
   onDeleteHackathon(filter: $filter) {
     createdAt
     endDate
@@ -330,10 +340,11 @@ export const onDeleteHackathon = /* GraphQL */ `subscription OnDeleteHackathon($
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnDeleteHackathonSubscriptionVariables,
-  APITypes.OnDeleteHackathonSubscription
->;
-export const onDeleteRoom = /* GraphQL */ `subscription OnDeleteRoom($filter: ModelSubscriptionRoomFilterInput) {
+    APITypes.OnDeleteHackathonSubscriptionVariables,
+    APITypes.OnDeleteHackathonSubscription
+  >;
+export const onDeleteRoom =
+  /* GraphQL */ `subscription OnDeleteRoom($filter: ModelSubscriptionRoomFilterInput) {
   onDeleteRoom(filter: $filter) {
     createdAt
     id
@@ -351,10 +362,11 @@ export const onDeleteRoom = /* GraphQL */ `subscription OnDeleteRoom($filter: Mo
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnDeleteRoomSubscriptionVariables,
-  APITypes.OnDeleteRoomSubscription
->;
-export const onDeleteScore = /* GraphQL */ `subscription OnDeleteScore($filter: ModelSubscriptionScoreFilterInput) {
+    APITypes.OnDeleteRoomSubscriptionVariables,
+    APITypes.OnDeleteRoomSubscription
+  >;
+export const onDeleteScore =
+  /* GraphQL */ `subscription OnDeleteScore($filter: ModelSubscriptionScoreFilterInput) {
   onDeleteScore(filter: $filter) {
     createdAt
     hackathon {
@@ -401,10 +413,11 @@ export const onDeleteScore = /* GraphQL */ `subscription OnDeleteScore($filter: 
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnDeleteScoreSubscriptionVariables,
-  APITypes.OnDeleteScoreSubscription
->;
-export const onDeleteTeam = /* GraphQL */ `subscription OnDeleteTeam($filter: ModelSubscriptionTeamFilterInput) {
+    APITypes.OnDeleteScoreSubscriptionVariables,
+    APITypes.OnDeleteScoreSubscription
+  >;
+export const onDeleteTeam =
+  /* GraphQL */ `subscription OnDeleteTeam($filter: ModelSubscriptionTeamFilterInput) {
   onDeleteTeam(filter: $filter) {
     approved
     createdAt
@@ -427,10 +440,11 @@ export const onDeleteTeam = /* GraphQL */ `subscription OnDeleteTeam($filter: Mo
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnDeleteTeamSubscriptionVariables,
-  APITypes.OnDeleteTeamSubscription
->;
-export const onDeleteTeamRoom = /* GraphQL */ `subscription OnDeleteTeamRoom($filter: ModelSubscriptionTeamRoomFilterInput) {
+    APITypes.OnDeleteTeamSubscriptionVariables,
+    APITypes.OnDeleteTeamSubscription
+  >;
+export const onDeleteTeamRoom =
+  /* GraphQL */ `subscription OnDeleteTeamRoom($filter: ModelSubscriptionTeamRoomFilterInput) {
   onDeleteTeamRoom(filter: $filter) {
     createdAt
     id
@@ -458,9 +472,9 @@ export const onDeleteTeamRoom = /* GraphQL */ `subscription OnDeleteTeamRoom($fi
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnDeleteTeamRoomSubscriptionVariables,
-  APITypes.OnDeleteTeamRoomSubscription
->;
+    APITypes.OnDeleteTeamRoomSubscriptionVariables,
+    APITypes.OnDeleteTeamRoomSubscription
+  >;
 export const onDeleteUser = /* GraphQL */ `subscription OnDeleteUser(
   $filter: ModelSubscriptionUserFilterInput
   $profileOwner: String
@@ -511,7 +525,8 @@ export const onDeleteUser = /* GraphQL */ `subscription OnDeleteUser(
   APITypes.OnDeleteUserSubscriptionVariables,
   APITypes.OnDeleteUserSubscription
 >;
-export const onDeleteUserFoodEventAttendance = /* GraphQL */ `subscription OnDeleteUserFoodEventAttendance(
+export const onDeleteUserFoodEventAttendance =
+  /* GraphQL */ `subscription OnDeleteUserFoodEventAttendance(
   $filter: ModelSubscriptionUserFoodEventAttendanceFilterInput
 ) {
   onDeleteUserFoodEventAttendance(filter: $filter) {
@@ -553,10 +568,11 @@ export const onDeleteUserFoodEventAttendance = /* GraphQL */ `subscription OnDel
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnDeleteUserFoodEventAttendanceSubscriptionVariables,
-  APITypes.OnDeleteUserFoodEventAttendanceSubscription
->;
-export const onUpdateFoodEvent = /* GraphQL */ `subscription OnUpdateFoodEvent($filter: ModelSubscriptionFoodEventFilterInput) {
+    APITypes.OnDeleteUserFoodEventAttendanceSubscriptionVariables,
+    APITypes.OnDeleteUserFoodEventAttendanceSubscription
+  >;
+export const onUpdateFoodEvent =
+  /* GraphQL */ `subscription OnUpdateFoodEvent($filter: ModelSubscriptionFoodEventFilterInput) {
   onUpdateFoodEvent(filter: $filter) {
     attended {
       nextToken
@@ -574,10 +590,11 @@ export const onUpdateFoodEvent = /* GraphQL */ `subscription OnUpdateFoodEvent($
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnUpdateFoodEventSubscriptionVariables,
-  APITypes.OnUpdateFoodEventSubscription
->;
-export const onUpdateHackathon = /* GraphQL */ `subscription OnUpdateHackathon($filter: ModelSubscriptionHackathonFilterInput) {
+    APITypes.OnUpdateFoodEventSubscriptionVariables,
+    APITypes.OnUpdateFoodEventSubscription
+  >;
+export const onUpdateHackathon =
+  /* GraphQL */ `subscription OnUpdateHackathon($filter: ModelSubscriptionHackathonFilterInput) {
   onUpdateHackathon(filter: $filter) {
     createdAt
     endDate
@@ -604,10 +621,11 @@ export const onUpdateHackathon = /* GraphQL */ `subscription OnUpdateHackathon($
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnUpdateHackathonSubscriptionVariables,
-  APITypes.OnUpdateHackathonSubscription
->;
-export const onUpdateRoom = /* GraphQL */ `subscription OnUpdateRoom($filter: ModelSubscriptionRoomFilterInput) {
+    APITypes.OnUpdateHackathonSubscriptionVariables,
+    APITypes.OnUpdateHackathonSubscription
+  >;
+export const onUpdateRoom =
+  /* GraphQL */ `subscription OnUpdateRoom($filter: ModelSubscriptionRoomFilterInput) {
   onUpdateRoom(filter: $filter) {
     createdAt
     id
@@ -625,10 +643,11 @@ export const onUpdateRoom = /* GraphQL */ `subscription OnUpdateRoom($filter: Mo
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnUpdateRoomSubscriptionVariables,
-  APITypes.OnUpdateRoomSubscription
->;
-export const onUpdateScore = /* GraphQL */ `subscription OnUpdateScore($filter: ModelSubscriptionScoreFilterInput) {
+    APITypes.OnUpdateRoomSubscriptionVariables,
+    APITypes.OnUpdateRoomSubscription
+  >;
+export const onUpdateScore =
+  /* GraphQL */ `subscription OnUpdateScore($filter: ModelSubscriptionScoreFilterInput) {
   onUpdateScore(filter: $filter) {
     createdAt
     hackathon {
@@ -675,10 +694,11 @@ export const onUpdateScore = /* GraphQL */ `subscription OnUpdateScore($filter: 
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnUpdateScoreSubscriptionVariables,
-  APITypes.OnUpdateScoreSubscription
->;
-export const onUpdateTeam = /* GraphQL */ `subscription OnUpdateTeam($filter: ModelSubscriptionTeamFilterInput) {
+    APITypes.OnUpdateScoreSubscriptionVariables,
+    APITypes.OnUpdateScoreSubscription
+  >;
+export const onUpdateTeam =
+  /* GraphQL */ `subscription OnUpdateTeam($filter: ModelSubscriptionTeamFilterInput) {
   onUpdateTeam(filter: $filter) {
     approved
     createdAt
@@ -701,10 +721,11 @@ export const onUpdateTeam = /* GraphQL */ `subscription OnUpdateTeam($filter: Mo
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnUpdateTeamSubscriptionVariables,
-  APITypes.OnUpdateTeamSubscription
->;
-export const onUpdateTeamRoom = /* GraphQL */ `subscription OnUpdateTeamRoom($filter: ModelSubscriptionTeamRoomFilterInput) {
+    APITypes.OnUpdateTeamSubscriptionVariables,
+    APITypes.OnUpdateTeamSubscription
+  >;
+export const onUpdateTeamRoom =
+  /* GraphQL */ `subscription OnUpdateTeamRoom($filter: ModelSubscriptionTeamRoomFilterInput) {
   onUpdateTeamRoom(filter: $filter) {
     createdAt
     id
@@ -732,9 +753,9 @@ export const onUpdateTeamRoom = /* GraphQL */ `subscription OnUpdateTeamRoom($fi
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnUpdateTeamRoomSubscriptionVariables,
-  APITypes.OnUpdateTeamRoomSubscription
->;
+    APITypes.OnUpdateTeamRoomSubscriptionVariables,
+    APITypes.OnUpdateTeamRoomSubscription
+  >;
 export const onUpdateUser = /* GraphQL */ `subscription OnUpdateUser(
   $filter: ModelSubscriptionUserFilterInput
   $profileOwner: String
@@ -785,7 +806,8 @@ export const onUpdateUser = /* GraphQL */ `subscription OnUpdateUser(
   APITypes.OnUpdateUserSubscriptionVariables,
   APITypes.OnUpdateUserSubscription
 >;
-export const onUpdateUserFoodEventAttendance = /* GraphQL */ `subscription OnUpdateUserFoodEventAttendance(
+export const onUpdateUserFoodEventAttendance =
+  /* GraphQL */ `subscription OnUpdateUserFoodEventAttendance(
   $filter: ModelSubscriptionUserFoodEventAttendanceFilterInput
 ) {
   onUpdateUserFoodEventAttendance(filter: $filter) {
@@ -827,6 +849,6 @@ export const onUpdateUserFoodEventAttendance = /* GraphQL */ `subscription OnUpd
   }
 }
 ` as GeneratedSubscription<
-  APITypes.OnUpdateUserFoodEventAttendanceSubscriptionVariables,
-  APITypes.OnUpdateUserFoodEventAttendanceSubscription
->;
+    APITypes.OnUpdateUserFoodEventAttendanceSubscriptionVariables,
+    APITypes.OnUpdateUserFoodEventAttendanceSubscription
+  >;
