6f0083c524874ed37e563106356838af0c3982b1
Fiona <fiona.truong@ucalgary.ca>
2025-02-02 22:40:46 -0700

    Added inner grey borders

--- src/components/reset/ResetPage.tsx
+++ src/components/reset/ResetPage.tsx
@@ -133,187 +133,197 @@ export default function ResetPage() {
       className="relative flex w-full flex-col justify-center gap-4 bg-dashboard-grey p-4 md:p-8"
     >
       <div className="flex w-full flex-col justify-between gap-2 md:gap-12">
-        <div className="m-4 flex w-full max-w-[1500px] flex-row justify-center rounded-md border border-awesomer-purple bg-light-grey p-5 text-lg text-black ">
-          <div className="mr-24 flex w-1/3 flex-col gap-2">
-            <Label>Scoring Components: </Label>
-            {scoringComponents.map((field, index) => (
-              <div key={field?.id} className="flex flex-row gap-2">
-                <Input
-                  required
-                  id="scoringComponents"
-                  {...register(`scoringComponents.${index}.friendlyName`)}
-                  placeholder="Idea, Effectiveness, Presentation, etc."
-                />
-                <Button
-                  type="button"
-                  variation="primary"
-                  colorTheme="error"
-                  onClick={() => removeScoringComponent(index)}
-                >
-                  -
-                </Button>
-              </div>
-            ))}
-            <Button
-              type="button"
-              variation="primary"
-              onClick={() =>
-                appendScoringComponent({
-                  friendlyName: "",
-                  isSidepot: false,
-                  id: generateId(),
-                })
-              }
-            >
-              Add Scoring Component
-            </Button>
-          </div>
-          <div className="flex w-1/3 flex-col gap-2">
-            <Label htmlFor="scoringSidepots">Scoring Sidepots: </Label>
-            {scoringSidepots.map((field, index) => (
-              <div key={field?.id} className="flex flex-row gap-2">
-                <Input
-                  required
-                  id="scoringSidepots"
-                  {...register(`scoringSidepots.${index}.friendlyName`)}
-                  placeholder="Creativity, Best UI, Use of AI, etc."
-                />
-                <Button
-                  variation="primary"
-                  colorTheme="error"
-                  type="button"
-                  onClick={() => removeScoringSidepot(index)}
-                >
-                  -
-                </Button>
-              </div>
-            ))}
-            <Button
-              type="button"
-              variation="primary"
-              onClick={() => {
-                appendScoringSidepot({
-                  friendlyName: "",
-                  isSidepot: true,
-                  id: generateId(),
-                });
-              }}
-            >
-              Add Scoring Sidepot
-            </Button>
+        <div className="m-4 flex w-full flex-row justify-center rounded-md border border-awesomer-purple bg-light-grey p-5 text-lg text-black">
+          <div className="m-1.8 flex w-full justify-center gap-8 bg-white p-10">
+            <div className="mr-24 flex w-1/3 flex-col gap-2">
+              <Label>Scoring Components: </Label>
+              {scoringComponents.map((field, index) => (
+                <div key={field?.id} className="flex flex-row gap-2">
+                  <Input
+                    required
+                    id="scoringComponents"
+                    {...register(`scoringComponents.${index}.friendlyName`)}
+                    placeholder="Idea, Effectiveness, Presentation, etc."
+                  />
+                  <Button
+                    type="button"
+                    variation="primary"
+                    colorTheme="error"
+                    onClick={() => removeScoringComponent(index)}
+                  >
+                    -
+                  </Button>
+                </div>
+              ))}
+              <Button
+                type="button"
+                variation="primary"
+                onClick={() =>
+                  appendScoringComponent({
+                    friendlyName: "",
+                    isSidepot: false,
+                    id: generateId(),
+                  })
+                }
+              >
+                Add Scoring Component
+              </Button>
+            </div>
+            <div className="flex w-1/3 flex-col gap-2">
+              <Label htmlFor="scoringSidepots">Scoring Sidepots: </Label>
+              {scoringSidepots.map((field, index) => (
+                <div key={field?.id} className="flex flex-row gap-2">
+                  <Input
+                    required
+                    id="scoringSidepots"
+                    {...register(`scoringSidepots.${index}.friendlyName`)}
+                    placeholder="Creativity, Best UI, Use of AI, etc."
+                  />
+                  <Button
+                    variation="primary"
+                    colorTheme="error"
+                    type="button"
+                    onClick={() => removeScoringSidepot(index)}
+                  >
+                    -
+                  </Button>
+                </div>
+              ))}
+              <Button
+                type="button"
+                variation="primary"
+                onClick={() => {
+                  appendScoringSidepot({
+                    friendlyName: "",
+                    isSidepot: true,
+                    id: generateId(),
+                  });
+                }}
+              >
+                Add Scoring Sidepot
+              </Button>
+            </div>
           </div>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-center">
           {" "}
-          <div className="m-4 mr-24 flex w-full min-w-96 max-w-[200px] flex-col gap-2 rounded-md border border-awesomer-purple bg-light-grey p-4 text-lg text-black">
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="startDate">Start Date:</Label>
-              <Input
-                required
-                type="date"
-                id="startDate"
-                placeholder="yyyy-mm-dd"
-                {...register("startDate")}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="endDate">End Date: </Label>
-              <Input
-                required
-                type="date"
-                id="endDate"
-                placeholder="yyyy-mm-dd"
-                {...register("endDate")}
-              />
+          <div className="m-4 mr-24 flex h-[350px] w-full min-w-96 max-w-[200px] flex-col gap-2 rounded-md border border-awesomer-purple bg-light-grey p-4 text-lg text-black">
+            <div className="m-1.8 h-full w-full flex-col justify-center gap-8 bg-white p-10 align-middle">
+              <div className="my-5 flex flex-col gap-2">
+                <Label htmlFor="startDate">Start Date:</Label>
+                <Input
+                  required
+                  type="date"
+                  id="startDate"
+                  placeholder="yyyy-mm-dd"
+                  {...register("startDate")}
+                />
+              </div>
+              <div className="my-5 flex flex-col gap-2 pt-5">
+                <Label htmlFor="endDate">End Date: </Label>
+                <Input
+                  required
+                  type="date"
+                  id="endDate"
+                  placeholder="yyyy-mm-dd"
+                  {...register("endDate")}
+                />
+              </div>
             </div>
           </div>{" "}
-          <div className="mr-24 flex min-w-20 flex-col gap-2">
-            <div className="flex flex-col gap-2">
-              <Label>Resetting or Creating Hackathon</Label>
-              <CheckboxField
-                label="Resetting Hackathon"
-                {...register("resetting")}
-                checked={resetting}
-                onChange={() => {
-                  handleCheckboxChange(
-                    "resetting" as keyof Schema["Hackathon"]["type"],
-                  );
-                }}
-              />
-              <CheckboxField
-                label="Creating Hackathon"
-                {...register("resetting")}
-                checked={creating}
-                onChange={() => {
-                  handleCheckboxChange(
-                    "creating" as keyof Schema["Hackathon"]["type"],
-                  );
-                  resetField("resetUsers");
-                  resetField("resetTeams");
-                  resetField("resetScores");
-                  resetField("resetRooms");
-                }}
-              />
+          <div className="h-[350px] min-w-[600px] max-w-[1000px] gap-2 rounded-md border border-awesomer-purple bg-light-grey p-4">
+            <div className="m-1.8 flex h-full w-full flex-row justify-center gap-20 bg-white p-10 py-20 align-middle">
+              <div className="flex min-w-20 flex-col">
+                <div className="flex flex-col gap-5">
+                  <Label>Resetting or Creating Hackathon</Label>
+                  <CheckboxField
+                    label="Resetting Hackathon"
+                    {...register("resetting")}
+                    checked={resetting}
+                    onChange={() => {
+                      handleCheckboxChange(
+                        "resetting" as keyof Schema["Hackathon"]["type"],
+                      );
+                    }}
+                  />
+                  <CheckboxField
+                    label="Creating Hackathon"
+                    {...register("resetting")}
+                    checked={creating}
+                    onChange={() => {
+                      handleCheckboxChange(
+                        "creating" as keyof Schema["Hackathon"]["type"],
+                      );
+                      resetField("resetUsers");
+                      resetField("resetTeams");
+                      resetField("resetScores");
+                      resetField("resetRooms");
+                    }}
+                  />
+                </div>
+              </div>
+              {resetting && (
+                <div className="flex w-1/3 flex-col gap-2">
+                  <Label>Reset Fields</Label>
+                  <CheckboxField
+                    label="Reset Users"
+                    {...register("resetUsers")}
+                    checked={watch("resetUsers")}
+                  />
+                  <CheckboxField
+                    label="Reset Teams"
+                    {...register("resetTeams")}
+                    checked={watch("resetTeams")}
+                  />
+                  <CheckboxField
+                    label="Reset Rooms"
+                    {...register("resetRooms")}
+                    checked={watch("resetRooms")}
+                  />
+                  <CheckboxField
+                    label="Reset Scores"
+                    {...register("resetScores")}
+                    checked={watch("resetScores")}
+                  />
+                </div>
+              )}
             </div>
           </div>
-          {resetting && (
-            <div className="flex w-1/3 flex-col gap-2">
-              <Label>Reset Fields</Label>
-              <CheckboxField
-                label="Reset Users"
-                {...register("resetUsers")}
-                checked={watch("resetUsers")}
-              />
-              <CheckboxField
-                label="Reset Teams"
-                {...register("resetTeams")}
-                checked={watch("resetTeams")}
-              />
-              <CheckboxField
-                label="Reset Rooms"
-                {...register("resetRooms")}
-                checked={watch("resetRooms")}
-              />
-              <CheckboxField
-                label="Reset Scores"
-                {...register("resetScores")}
-                checked={watch("resetScores")}
-              />
-            </div>
-          )}
         </div>
-        <div className="m-4 flex w-full max-w-[1500px] flex-col gap-2 rounded-md border border-awesomer-purple bg-light-grey p-4 text-lg text-black">
-          <Label htmlFor="safetyCheck">
-            Enter &quot;i love code the change&quot; to confirm{" "}
-          </Label>
+        <div className="m-4 mr-auto flex min-w-[700px] max-w-[1000px] flex-col items-center justify-center gap-2 rounded-md border border-awesomer-purple bg-light-grey p-4 text-lg text-black">
+          <div className="m-1.8 w-full flex-col justify-center gap-8 bg-white p-10">
+            <Label htmlFor="safetyCheck">
+              Enter &quot;i love code the change&quot; to confirm{" "}
+            </Label>
 
-          <div className="flex w-1/2 flex-row gap-2">
-            <Input
-              required
-              id="safetyCheck"
-              placeholder="i love code the change"
-              {...register("safetyCheck")}
-              className="w-20"
-            />
-            <Button
-              variation="primary"
-              colorTheme={
-                userMutation.isSuccess
-                  ? "success"
+            <div className="mt-5 flex w-1/2 flex-row gap-2">
+              <Input
+                required
+                id="safetyCheck"
+                placeholder="i love code the change"
+                {...register("safetyCheck")}
+                className="w-20"
+              />
+              <Button
+                variation="primary"
+                colorTheme={
+                  userMutation.isSuccess
+                    ? "success"
+                    : userMutation.isError
+                      ? "error"
+                      : undefined // Set the colorTheme to undefined if none of the conditions are met
+                }
+                loadingText="Loading..."
+                type="submit"
+                isLoading={userMutation.isPending}
+              >
+                {userMutation.isSuccess
+                  ? "Success"
                   : userMutation.isError
-                    ? "error"
-                    : undefined // Set the colorTheme to undefined if none of the conditions are met
-              }
-              loadingText="Loading..."
-              type="submit"
-              isLoading={userMutation.isPending}
-            >
-              {userMutation.isSuccess
-                ? "Success"
-                : userMutation.isError
-                  ? "Error"
-                  : "Submit"}
-            </Button>
+                    ? "Error"
+                    : "Submit"}
+              </Button>
+            </div>
           </div>
         </div>
       </div>
