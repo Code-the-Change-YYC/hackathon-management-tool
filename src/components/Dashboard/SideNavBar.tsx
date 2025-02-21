c051a72d8ebdca15a82235927afc209f3191bd4a
fionaa-truong <151110138+fionaa-truong@users.noreply.github.com>
2025-02-21 01:05:36 -0700

    Updated z-index for overlap

--- src/components/Dashboard/SideNavBar.tsx
+++ src/components/Dashboard/SideNavBar.tsx
@@ -105,7 +105,7 @@ export default function SideNavBar() {
 
   return (
     <details
-      className={`transition-width md:open:w-80" relative flex h-full w-20 cursor-pointer flex-col items-center gap-2 bg-awesomer-purple text-white duration-500 open:fixed open:top-0 open:w-60 md:w-[80px]`}
+      className={`z-10 transition-width md:open:w-80" relative flex h-full w-20 cursor-pointer flex-col items-center gap-2 bg-awesomer-purple text-white duration-500 open:fixed open:top-0 open:w-60 md:w-[80px]`}
     >
       <summary className="sticky top-0 list-none pt-4">
         <Image
