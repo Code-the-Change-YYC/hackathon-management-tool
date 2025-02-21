e60da58357b475ab93cbfbe50f086272a11dc594
fionaa-truong <151110138+fionaa-truong@users.noreply.github.com>
2025-02-21 01:57:39 -0700

    "prettier" again

--- src/components/Dashboard/SideNavBar.tsx
+++ src/components/Dashboard/SideNavBar.tsx
@@ -105,7 +105,7 @@ export default function SideNavBar() {
 
   return (
     <details
-      className={`transition-width md:open:w-80" relative flex h-full w-20 cursor-pointer flex-col items-center gap-2 bg-awesomer-purple text-white duration-500 open:fixed open:top-0 open:w-60 md:w-[80px] z-10`}
+      className={`transition-width md:open:w-80" relative flex h-full w-20 cursor-pointer flex-col items-center gap-2 bg-awesomer-purple text-white duration-500 z-10 open:fixed open:top-0 open:w-60 md:w-[80px]`}
     >
       <summary className="sticky top-0 list-none pt-4">
         <Image
