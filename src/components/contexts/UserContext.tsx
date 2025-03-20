3e1ea836381c90e5bae404ddab8ff03967d72589
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2025-03-19 23:11:11 -0600

    missing fields added to the IUserType

--- src/components/contexts/UserContext.tsx
+++ src/components/contexts/UserContext.tsx
@@ -20,29 +20,30 @@ export enum UserType {
   Guest = "Guest",
 }
 
-export interface IUser {
-  username: string;
+type StripFunctions<T> = {
+  [K in keyof T as T[K] extends Function ? never : K]: T[K];
+};
+export type IUser = StripFunctions<Schema["User"]["type"]> & {
   type: UserType;
-  completedProfile?: boolean;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  teamId?: string;
-  populated: boolean;
-  JUDGE_roomId: string;
-}
+  username: string;
+};
 
 interface IUserReturn {
   currentUser: IUser;
   revalidateUser: () => void;
   isFetching: boolean;
-  // setCurrentUser: (state: IUser) => void;
 }
 
+const GUEST_USER: IUser = {
+  id: "",
+  createdAt: "",
+  updatedAt: "",
+  username: "",
+  type: UserType.Guest,
+};
 const client = generateClient<Schema>();
 
 export const UserContext = createContext<IUserReturn>({} as IUserReturn);
-
 export function UserContextProvider({ children }: Props) {
   const queryClient = useQueryClient();
 
@@ -51,12 +52,7 @@ export function UserContextProvider({ children }: Props) {
     refetch,
     isFetching,
   } = useQuery({
-    initialData: {
-      username: "",
-      type: UserType.Guest,
-      populated: false,
-      JUDGE_roomId: "",
-    },
+    initialData: GUEST_USER,
     queryKey: ["User"],
     queryFn: async () => {
       try {
@@ -87,19 +83,12 @@ export function UserContextProvider({ children }: Props) {
             signOut();
             console.error("User not in DB");
           }
-
           return {
+            ...response.data,
             username: user.userSub as string,
             type: (
               user.tokens?.idToken?.payload["cognito:groups"] as UserType[]
             ).filter((group) => Object.keys(UserType).includes(group))?.[0],
-            populated: true,
-            completedProfile: response.data?.completedRegistration ?? false,
-            email: response.data?.email ?? "",
-            teamId: response.data?.teamId ?? "",
-            firstName: response.data?.firstName ?? "",
-            lastName: response.data?.lastName ?? "",
-            JUDGE_roomId: response.data?.JUDGE_roomId,
           } as IUser;
         } catch (error) {
           console.error(error);
@@ -108,11 +97,7 @@ export function UserContextProvider({ children }: Props) {
       } catch (error) {
         if (String(error).includes("No user")) {
           console.info("Not Logged in");
-          return {
-            username: "",
-            type: UserType.Guest,
-            populated: true,
-          } as IUser;
+          return GUEST_USER;
         } else {
           console.error(error);
         }
@@ -130,11 +115,7 @@ export function UserContextProvider({ children }: Props) {
           console.log("Signed In");
           break;
         case "signedOut":
-          queryClient.setQueryData(["User"], {
-            username: "",
-            type: UserType.Guest,
-            populated: true,
-          } as IUser);
+          queryClient.setQueryData(["User"], GUEST_USER);
           console.log("Signed Out");
           break;
       }
