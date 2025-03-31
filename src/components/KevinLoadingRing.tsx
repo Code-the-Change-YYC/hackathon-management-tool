e293518b8373631a12920b788dbc51ac8d39eae1
sim <simarkandola51@gmail.com>
2025-03-31 17:58:11 -0600

    correct score mapping on judge dash (#192)

---
+++ src/components/KevinLoadingRing.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function KevinLoadingRing() {
+  return (
+    <>
+      <div className="m-4 flex h-full flex-col justify-center text-center text-2xl">
+        <Image
+          className="animate-bounce"
+          src="/svgs/admin/Kevin.svg"
+          alt="Kevin Icon"
+          width={240}
+          height={240}
+        />
+        <div className="ml-2 flex w-full items-center justify-center space-x-1">
+          <span className="text-dark-pink">Loading</span>
+          <span className=" mt-3 size-1 animate-pulse rounded-full bg-dark-pink delay-200"></span>
+          <span className="delay-400 mt-3 size-1 animate-pulse rounded-full bg-dark-pink"></span>
+          <span className="delay-600 mt-3 size-1 animate-pulse rounded-full bg-dark-pink"></span>
+        </div>
+      </div>
+    </>
+  );
+}
