597d2ab6a9930e008c41d9feae80b590e8e45509
anthonyych4n <anthony168c@gmail.com>
2025-03-30 18:56:27 -0600

    deleted comments and fixed tailwind

--- src/components/judging/Navbar.tsx
+++ src/components/judging/Navbar.tsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Navigation links
   const navigation = [
     {
       name: "Dashboard",
@@ -32,14 +31,12 @@ export default function NavBar() {
 
   return (
     <>
-      {/* Hamburger button - absolutely positioned in the top left */}
       {!isMenuOpen && (
         <button
           onClick={toggleMenu}
           className="absolute left-8 top-12 z-50 p-2 text-awesomer-purple"
           aria-label="Toggle menu"
         >
-          {/* CSS-only hamburger icon */}
           <div className="relative flex size-8 flex-col justify-center space-y-1.5">
             <span className="block h-[2.5px] w-8 bg-current"></span>
             <span className="block h-[2.5px] w-8 bg-current"></span>
@@ -48,23 +45,15 @@ export default function NavBar() {
         </button>
       )}
 
-      {/* Mobile menu overlay */}
       {isMenuOpen && (
-        <div
-          // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
-          className="fixed inset-0 z-40 bg-black bg-opacity-50"
-          onClick={toggleMenu}
-        />
+        <div className="fixed inset-0 z-40 bg-black/50" onClick={toggleMenu} />
       )}
 
-      {/* Navigation menu - slides in from left */}
       <div
-        // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
-        className={`fixed left-0 top-0 z-40 h-full w-64 transform bg-white shadow-lg transition-transform duration-300 ease-in-out ${
+        className={`fixed left-0 top-0 z-40 h-full w-64 bg-white shadow-lg transition-transform duration-300 ease-in-out ${
           isMenuOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        {/* Close (X) icon inside the navigation menu */}
         <button
           onClick={toggleMenu}
           className="absolute left-4 top-4 z-50 p-2 text-awesomer-purple"
@@ -76,7 +65,6 @@ export default function NavBar() {
           </div>
         </button>
 
-        {/* Navigation links */}
         <div className="flex flex-col px-4 pt-20">
           {navigation.map((item) => (
             <Link
