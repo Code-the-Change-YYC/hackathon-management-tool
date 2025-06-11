9240076b23a2e28c9dcc88531fe4e33c837a0496
anthonyych4n <anthony168c@gmail.com>
2025-06-11 01:52:07 -0600

    i think this fixes it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -64,7 +64,8 @@ export default function Header() {
               </>
             ) : user.type === UserType.Admin ? (
               <Link href="/admin">Admin Dashboard</Link>
-            ) : user.type === UserType.Judge ? ( //Probably change the conditon here to make sure it redirects to the judging page only if the user is a judge regardless of TEAMID
+            ) : user.type === UserType.Judge ||
+              (UserType.Judge && user.teamId) ? (
               <Link href="/judging"></Link>
             ) : null}
           </>
