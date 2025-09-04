a5c164cf95a5dc6985006f3375fef96b851bb4c4
anthonyych4n <anthony168c@gmail.com>
2025-09-04 04:58:17 -0600

    fix

--- src/app/participant/team-selection/page.tsx
+++ src/app/participant/team-selection/page.tsx
@@ -1,5 +1,9 @@
 import CreateTeamPage from "@/app/participant/team-selection/CreateTeamPage";
 
 export default function TeamSelection() {
-  return <main></main>;
+  return (
+    <main>
+      <CreateTeamPage />
+    </main>
+  );
 }
